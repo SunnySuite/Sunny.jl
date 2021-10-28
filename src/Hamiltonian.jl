e12d24d34e60330ee71273add23f945bb50b877c
Kipton Barros <kbarros@gmail.com>
2021-10-27 20:01:31 -0600

    Move validation of interactions to Hamiltonian level

--- src/Hamiltonian.jl
+++ src/Hamiltonian.jl
@@ -1,17 +1,63 @@
-"""
-Defines HamiltonianCPU which maintains the actual internal interaction
-types and orchestrates energy/field calculations.
-"""
+# Functions associated with HamiltonianCPU, which maintains the actual internal
+# interaction types and orchestrates energy/field calculations.
 
-function validate_dimensionality(ints::Vector{<:Interaction}, D::Int)
-    for int in ints
-        if isa(int, QuadraticInteraction) && typeof(int).parameters[1] != D
-            error("One of the provided pair interactions is not the correct dimensionality.")
-        elseif isa(int, DipoleDipole) && D != 3
-            error("Dipole-dipole interactions only supported for D = 3.")
+
+function validate_and_clean_interactions(ints::Vector{Interaction}, crystal::Crystal, latsize::Vector{Int64})
+    D = dimension(crystal)
+
+    # Now that we know dimension D, we can convert every OnSiteQuadratic to
+    # QuadraticInteraction
+    ints = map(ints) do int
+        if isa(int, OnSiteQuadratic)
+            return QuadraticInteraction(int.J, Bond{D}(int.site, int.site, zeros(D)), int.label)
+        else
+            return int
         end
     end
+
+    # Validate all interactions
+    for int in ints
+        if isa(int, QuadraticInteraction)
+            b = int.bond
+
+            # Verify that the dimension is correct
+            if length(b.n) != D
+                error("Interaction $(repr(MIME("text/plain"), int)) inconsistent with crystal dimension $D.")
+            end
+
+            # Verify that the interactions are symmetry-consistent
+            if !is_coupling_valid(crystal, b, int.J)
+                println("Symmetry-violating interaction: $(repr(MIME("text/plain"), int)).")
+                if b.i == b.j && iszero(b.n)
+                    println("Allowed single-ion anisotropy for this atom:")
+                else
+                    println("Allowed exchange for this bond:")
+                end
+                print_allowed_coupling(crystal, b; prefix="    ")
+                println("Use `print_bond(crystal, bond)` for more information.")
+                error("Interaction violates symmetry.")
+            end
+
+            # Verify that no bond wraps the entire system
+            bs = all_symmetry_related_bonds(crystal, b)
+            wraps = any(bs) do b
+                any(abs.(b.n) .>= latsize)
+            end
+            if wraps
+                println("Distance-violating interaction: $int.")
+                error("Interaction wraps system.")
+            end
+
+        elseif isa(int, DipoleDipole)
+            if D != 3
+                error("Dipole-dipole interactions require three dimensions.")
+            end
+        end
+    end
+
+    return ints
 end
+
 
 """
     HamiltonianCPU{D}
@@ -34,15 +80,14 @@ Construct a `HamiltonianCPU{3}` from a list of interactions, converting
 each of the interactions into the proper backend type specialized
 for the given `crystal` and `latsize`.
 """
-function HamiltonianCPU(ints::Vector{<:Interaction}, crystal::Crystal, latsize)
+function HamiltonianCPU(ints::Vector{<:Interaction}, crystal::Crystal, latsize::Vector{Int64})
     ext_field   = nothing
     heisenbergs = Vector{HeisenbergCPU{3}}()
     diag_coups  = Vector{DiagonalCouplingCPU{3}}()
     gen_coups   = Vector{GeneralCouplingCPU{3}}()
     dipole_int  = nothing
 
-    D = dimension(crystal)
-    validate_dimensionality(ints, D)
+    ints = validate_and_clean_interactions(ints, crystal, latsize)
 
     for int in ints
         if isa(int, ExternalField)
