dfefecc0ccb25aed7d7df5d78ead9a26466a7382
mswwilson <msw.wilson@uga.edu>
2021-11-04 21:40:32 -0400

    Added Kipton's spherical cap update and include Gaussian spin update. (#3)
    * In progress
    
    * In progress

--- examples/WL_afm_diamond.jl
+++ examples/WL_afm_diamond.jl
@@ -6,7 +6,7 @@ crystal = Sunny.diamond_crystal(a=1.0)
 ## Interactions -- units of K           
 J = 28.28 
 interactions = [
-	heisenberg(J, Bond{3}(3, 6, [0,0,0])),
+    heisenberg(J, Bond{3}(3, 6, [0,0,0])),
 ]
 
 ## Spin system
@@ -24,13 +24,14 @@ ps = true
 
 WL = WangLandau(
     system=sys, 
-	bin_size = bs,
-	hcheck_interval=10_000,
-	bounds=[E_min, E_max], 
-	mc_step_size=0.1,
-	flatness=0.6,
-	ln_f_final=1e-6,
-	per_spin=ps
+    bin_size = bs,
+    hcheck_interval=10_000,
+    bounds=[E_min, E_max], 
+    mc_step_size=0.2,
+    flatness=0.6,
+    ln_f_final=1e-6,
+    per_spin=ps,
+    max_mcsweeps=10_000_000_000
 )
 
 ## Run WL simulation
