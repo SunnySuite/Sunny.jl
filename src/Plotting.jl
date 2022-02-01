48181a8119f68e66177d6d28132373aec7bafa09
Cole Miles <cmm572@cornell.edu>
2022-02-01 00:54:36 -0500

    Fix plotting bugs +cleanup plotting code.

--- src/Plotting.jl
+++ src/Plotting.jl
@@ -6,13 +6,10 @@ function plot_lattice!(ax, lattice::Lattice; colors=:Set1_9, markersize=200, lin
     colors = GLMakie.to_colormap(colors, 9)
 
     # Plot markers at each site
-    sites = reinterpret(reshape, Float64, collect(lattice))
+    pts = GLMakie.Point3f.(vec(lattice))
     for (i, type) in enumerate(unique_types)
         basis_idxs = findall(isequal(type), lattice.types)
-        xs = vec(sites[1, basis_idxs, 1:end, 1:end, 1:end])
-        ys = vec(sites[2, basis_idxs, 1:end, 1:end, 1:end])
-        zs = vec(sites[3, basis_idxs, 1:end, 1:end, 1:end])
-        GLMakie.scatter!(ax, xs, ys, zs; label=type, color=colors[i], markersize=markersize, show_axis=false, kwargs...)
+        GLMakie.scatter!(ax, pts; label=type, color=colors[i], markersize=markersize, kwargs...)
     end
 
     # For some odd reason, the sites will not appear unless this happens afterwards
@@ -20,18 +17,18 @@ function plot_lattice!(ax, lattice::Lattice; colors=:Set1_9, markersize=200, lin
     plot_cells!(ax, lattice; color=linecolor, linewidth=linewidth)
 end
 
-function _setup_scene()
-    f = GLMakie.Figure()
-    lscene = GLMakie.LScene(f[1, 1], scenekw=(camera=GLMakie.cam3d!, raw=false))
-    return f, lscene
+function _setup_scene(; show_axis=false)
+    fig = GLMakie.Figure()
+    ax = GLMakie.LScene(fig[1, 1], show_axis=show_axis)
+    return fig, ax
 end
 
 function plot_lattice(lattice::Lattice; kwargs...)
-    f, lscene = _setup_scene()
-    plot_lattice!(lscene, lattice; kwargs...)
+    fig, ax = _setup_scene()
+    plot_lattice!(ax, lattice; kwargs...)
     # TODO: Markers are often way too big.
-    f[1, 2] = GLMakie.Legend(f, lscene, "Species")
-    f
+    fig[1, 2] = GLMakie.Legend(fig, ax, "Species")
+    fig
 end
 
 """
@@ -50,17 +47,11 @@ colors=:Set1_9, markersize=20, linecolor=:grey, linewidth=1.0, kwargs...
 Additional keyword arguments are given to `GLMakie.scatter!` which
 draws the points.
 """
-function plot_lattice(cryst::Crystal, latsize=(3,3,3); kwargs...)
-    f, ax = _setup_scene()
-    lattice = Lattice(cryst, latsize)
-    plot_lattice!(ax, lattice; kwargs...)
-    f[1, 2] = GLMakie.Legend(f, ax, "Species")
-    f
-end
+plot_lattice(cryst::Crystal, latsize=(3,3,3); kwargs...) = plot_lattice(Lattice(cryst, latsize); kwargs...)
 
-function plot_bonds(lattice::Lattice, ints::Vector{<:PairInt};
+function plot_bonds(lattice::Lattice, ints::Vector{<:InteractionCPU};
                     colors=:Dark2_8, bondwidth=4, kwargs...)
-    f, ax = _setup_scene()
+    fig, ax = _setup_scene()
 
     # Plot the bonds, all relative to a central atom on the first sublattice
     # TODO: Make selectable in GUI
@@ -81,35 +72,33 @@ function plot_bonds(lattice::Lattice, ints::Vector{<:PairInt};
     cent_cell = CartesianIndex(div.(lattice.size .+ 1, 2)...)
     cent_pt = lattice[basis_idx, cent_cell]
     for (n, int) in enumerate(ints)
-        xs = Vector{Float64}()
-        ys = Vector{Float64}()
-        zs = Vector{Float64}()
+        if !isa(int, PairInt)
+            continue
+        end
+
+        pts = Vector{GLMakie.Point3f}()
         for bond in int.bonds[basis_idx]
             new_cell = offset(cent_cell, bond.n, lattice.size)
             bond_pt = lattice[bond.j, new_cell]
-            push!(xs, cent_pt[1])
-            push!(ys, cent_pt[2])
-            push!(zs, cent_pt[3])
-            push!(xs, bond_pt[1])
-            push!(ys, bond_pt[2])
-            push!(zs, bond_pt[3])
+            push!(pts, GLMakie.Point3f(cent_pt))
+            push!(pts, GLMakie.Point3f(bond_pt))
         end
-        if length(xs) == 0
+        if length(pts) == 0
             continue
         end
 
         color = colors[mod1(n, 8)]
-        seg = GLMakie.linesegments!(xs, ys, zs; linewidth=bondwidth, label=int.label, color=color)
-        tog = GLMakie.Toggle(f, active=true)
+        seg = GLMakie.linesegments!(pts; linewidth=bondwidth, label=int.label, color=color)
+        tog = GLMakie.Toggle(fig, active=true)
         GLMakie.connect!(seg.visible, tog.active)
         push!(toggles, tog)
-        push!(labels, GLMakie.Label(f, int.label))
+        push!(labels, GLMakie.Label(fig, int.label))
     end
     GLMakie.axislegend()
     if length(toggles) > 0
-        f[1, 2] = GLMakie.grid!(hcat(toggles, labels), tellheight=false)
+        fig[1, 2] = GLMakie.grid!(hcat(toggles, labels), tellheight=false)
     end
-    f
+    fig
 end
 
 
@@ -119,11 +108,10 @@ end
 Plot a list of pair interactions defined on a `Crystal`. `latsize` sets how many
 unit cells are plotted, and `kwargs` are passed to to `plot_lattice!`.
 """
-function plot_bonds(cryst::Crystal, ints::Vector{<:Interaction}, latsize=(3,3,3),
-                    sites_info::Vector{SiteInfo}=SiteInfo[]; kwargs...)
+function plot_bonds(cryst::Crystal, ints::Vector{<:Interaction}, latsize=(3,3,3); kwargs...)
     latsize = collect(Int64.(latsize))
     lattice = Lattice(cryst, latsize)
-    all_sites_info = propagate_site_info(cryst, sites_info)
+    all_sites_info = propagate_site_info(cryst, SiteInfo[])
     ℋ = HamiltonianCPU(ints, cryst, latsize, all_sites_info)
     pair_ints = vcat(ℋ.heisenbergs, ℋ.diag_coups, ℋ.gen_coups)
     plot_bonds(lattice, pair_ints; kwargs...)
@@ -136,7 +124,8 @@ end
 Plot all pair interactions appearing in `sys.hamiltonian`, on the
 underlying crystal lattice. `kwargs` are passed to `plot_lattice!`.
 """
-@inline function plot_bonds(sys::SpinSystem; kwargs...)
+@inline function plot_bonds(sys::SpinSystem; reduced_cell=true, kwargs...)
+    ℋ = sys.hamiltonian
     pair_ints = vcat(ℋ.heisenbergs, ℋ.diag_coups, ℋ.gen_coups)
     plot_bonds(sys.lattice, pair_ints; kwargs...)
 end
@@ -150,7 +139,7 @@ plotted along each axis. `kwargs` are passed to `plot_bonds`.
 """
 function plot_all_bonds(crystal::Crystal, max_dist, latsize=(3,3,3); kwargs...)
     ref_bonds = reference_bonds(crystal, max_dist)
-    interactions = Interaction[]
+    interactions = QuadraticInteraction[]
     
     prev_dist = 0.0
     dist = 0
@@ -170,16 +159,14 @@ function plot_all_bonds(crystal::Crystal, max_dist, latsize=(3,3,3); kwargs...)
     end
 
     @assert length(interactions) > 0 "No non-self interactions found!"
-    # Sort interactions so that longer bonds are plotted first
-    sort!(interactions, by=int->distance(crystal, int.bonds[1][1]))
 
-    plot_bonds(crystal, interactions, latsize, kwargs...)
+    plot_bonds(crystal, interactions, latsize; kwargs...)
 end
 
 "Plot all bonds between equivalent sites i and j"
-function plot_all_bonds_between(crystal, i, j, max_dist, latsize=(3,3,3), kwargs...)
+function plot_all_bonds_between(crystal, i, j, max_dist, latsize=(3,3,3); kwargs...)
     ref_bonds = reference_bonds(crystal, max_dist)
-    interactions = Vector{HeisenbergCPU}()
+    interactions = QuadraticInteraction[]
 
     prev_dist = 0.0
     dist = 0
@@ -196,13 +183,11 @@ function plot_all_bonds_between(crystal, i, j, max_dist, latsize=(3,3,3), kwargs
                 class = 1
             end
             label = "J$(dist)_$(class)"
-            push!(interactions, HeisenbergCPU(1.0, crystal, bond, label))
+            push!(interactions, heisenberg(1.0, bond, label))
         end
     end
 
     @assert length(interactions) > 0 "No non-self interactions found!"
-    # Sort interactions so that longer bonds are plotted first
-    sort!(interactions, by=int->distance(lattice, int.bonds[1]))
 
     plot_bonds(crystal, interactions, latsize; kwargs...)
 end
@@ -211,60 +196,44 @@ end
 function plot_cells!(ax, lattice::Lattice; color=:grey, linewidth=1.0, kwargs...)
     lattice = brav_lattice(lattice)
 
-    xs, ys, zs = Vector{Float64}(), Vector{Float64}(), Vector{Float64}()
+    pts = Vector{GLMakie.Point3f}()
     nx, ny, nz = lattice.size
     for j in 1:ny
         for k in 1:nz
             bot_pt, top_pt = lattice[1, 1, j, k], lattice[1, nx, j, k]
-            push!(xs, bot_pt[1])
-            push!(xs, top_pt[1])
-            push!(ys, bot_pt[2])
-            push!(ys, top_pt[2])
-            push!(zs, bot_pt[3])
-            push!(zs, top_pt[3])
+            push!(pts, GLMakie.Point3f(bot_pt))
+            push!(pts, GLMakie.Point3f(top_pt))
         end
         for i in 1:nx
             left_pt, right_pt = lattice[1, i, j, 1], lattice[1, i, j, nz]
-            push!(xs, left_pt[1])
-            push!(xs, right_pt[1])
-            push!(ys, left_pt[2])
-            push!(ys, right_pt[2])
-            push!(zs, left_pt[3])
-            push!(zs, right_pt[3])
+            push!(pts, GLMakie.Point3f(left_pt))
+            push!(pts, GLMakie.Point3f(right_pt))
         end
     end
     for k in 1:nz
         for i in 1:nx
             left_pt, right_pt = lattice[1, i, 1, k], lattice[1, i, ny, k]
-            push!(xs, left_pt[1])
-            push!(xs, right_pt[1])
-            push!(ys, left_pt[2])
-            push!(ys, right_pt[2])
-            push!(zs, left_pt[3])
-            push!(zs, right_pt[3])
+            push!(pts, GLMakie.Point3f(left_pt))
+            push!(pts, GLMakie.Point3f(right_pt))
         end
     end
 
-    GLMakie.linesegments!(ax, xs, ys, zs; color=color, linewidth=linewidth)
+    GLMakie.linesegments!(ax, pts; color=color, linewidth=linewidth)
 end
 
 function plot_spins(lat::Lattice, spins; linecolor=:grey, arrowcolor=:red,
-                    linewidth=0.1, arrowsize=0.3, arrowlength=1.0, kwargs...)
-    sites = reinterpret(reshape, Float64, collect(lat))
-    spins = reinterpret(reshape, Float64, spins.val)
+                    linewidth=0.1, arrowsize=0.2, arrowlength=0.2, kwargs...)
+    fig, ax = _setup_scene()
 
-    xs = vec(sites[1, 1:end, 1:end, 1:end, 1:end])
-    ys = vec(sites[2, 1:end, 1:end, 1:end, 1:end])
-    zs = vec(sites[3, 1:end, 1:end, 1:end, 1:end])
-    us = arrowlength * vec(spins[1, 1:end, 1:end, 1:end, 1:end])
-    vs = arrowlength * vec(spins[2, 1:end, 1:end, 1:end, 1:end])
-    ws = arrowlength * vec(spins[3, 1:end, 1:end, 1:end, 1:end])
+    pts = GLMakie.Point3f.(vec(lat))
+    vecs = GLMakie.Vec3f.(vec(spins))
 
-    GLMakie.arrows(
-        xs, ys, zs, us, vs, ws;
+    GLMakie.arrows!(
+        ax, pts, vecs;
         linecolor=linecolor, arrowcolor=arrowcolor, linewidth=linewidth, arrowsize=arrowsize,
-        show_axis=false, kwargs...    
+        lengthscale=arrowlength, kwargs...    
     )
+    fig
 end
 
 """
@@ -292,21 +261,17 @@ Other keyword arguments are passed to `GLMakie.arrows`.
 """
 function anim_integration(
     sys::SpinSystem, fname, steps_per_frame, Δt, nframes;
-    linecolor=:grey, arrowcolor=:red, linewidth=0.1, arrowsize=0.2, kwargs...
+    linecolor=:grey, arrowcolor=:red, linewidth=0.1, arrowsize=0.2, arrowlength=0.2,
+    kwargs...
 )
-    sites = reinterpret(reshape, Float64, collect(sys.lattice))
-    spins = 0.2 .* reinterpret(reshape, Float64, sys.sites)
-    
-    xs = vec(sites[1, 1:end, 1:end, 1:end, 1:end])
-    ys = vec(sites[2, 1:end, 1:end, 1:end, 1:end])
-    zs = vec(sites[3, 1:end, 1:end, 1:end, 1:end])
-    us = GLMakie.Node(vec(spins[1, 1:end, 1:end, 1:end, 1:end]))
-    vs = GLMakie.Node(vec(spins[2, 1:end, 1:end, 1:end, 1:end]))
-    ws = GLMakie.Node(vec(spins[3, 1:end, 1:end, 1:end, 1:end]))
-    fig, ax, plot = GLMakie.arrows(
-        xs, ys, zs, us, vs, ws;
+    fig, ax = _setup_scene()
+
+    pts = GLMakie.Point3f.(vec(sys.lattice))
+    vecs = GLMakie.Observable(GLMakie.Vec3f.(vec(sys)))
+    GLMakie.arrows!(
+        ax, pts, vecs;
         linecolor=linecolor, arrowcolor=arrowcolor, linewidth=linewidth, arrowsize=arrowsize,
-        show_axis=false, kwargs...    
+        lengthscale=arrowlength, kwargs...    
     )
     display(fig)
 
@@ -317,10 +282,7 @@ function anim_integration(
         for step in 1:steps_per_frame
             evolve!(integrator, Δt)
         end
-        spins = 0.2 .* reinterpret(reshape, Float64, sys.sites)
-        us[] = vec(spins[1, 1:end, 1:end, 1:end, 1:end])
-        vs[] = vec(spins[2, 1:end, 1:end, 1:end, 1:end])
-        ws[] = vec(spins[3, 1:end, 1:end, 1:end, 1:end])
+        vecs[] = GLMakie.Vec3f.(vec(sys))
     end
 end
 
@@ -333,35 +295,26 @@ in an interactive window.
 function live_integration(
     sys::SpinSystem, steps_per_frame, Δt;
     linecolor=:grey, arrowcolor=:red, linewidth=0.1, arrowsize=0.2,
-    arrowlength=1.0, kwargs...
+    arrowlength=0.2, framerate=30, kwargs...
 )
-    sites = reinterpret(reshape, Float64, collect(sys.lattice))
-    spins = 0.2 .* reinterpret(reshape, Float64, sys.sites)
-    
-    xs = vec(sites[1, 1:end, 1:end, 1:end, 1:end])
-    ys = vec(sites[2, 1:end, 1:end, 1:end, 1:end])
-    zs = vec(sites[3, 1:end, 1:end, 1:end, 1:end])
-    us = GLMakie.Node(arrowlength * vec(spins[1, 1:end, 1:end, 1:end, 1:end]))
-    vs = GLMakie.Node(arrowlength * vec(spins[2, 1:end, 1:end, 1:end, 1:end]))
-    ws = GLMakie.Node(arrowlength * vec(spins[3, 1:end, 1:end, 1:end, 1:end]))
-    fig, ax, plot = GLMakie.arrows(
-        xs, ys, zs, us, vs, ws;
+    fig, ax = _setup_scene()
+
+    pts = GLMakie.Point3f.(vec(sys.lattice))
+    vecs = GLMakie.Observable(GLMakie.Vec3f.(vec(sys)))
+    GLMakie.arrows!(
+        ax, pts, vecs;
         linecolor=linecolor, arrowcolor=arrowcolor, linewidth=linewidth, arrowsize=arrowsize,
-        show_axis=false, kwargs...    
+        lengthscale=arrowlength, kwargs...    
     )
     display(fig)
 
-    framerate = 30
     integrator = HeunP(sys)
 
     while true
         for step in 1:steps_per_frame
             evolve!(integrator, Δt)
         end
-        spins = 0.2 .* reinterpret(reshape, Float64, sys.sites)
-        us[] = arrowlength * vec(spins[1, 1:end, 1:end, 1:end, 1:end])
-        vs[] = arrowlength * vec(spins[2, 1:end, 1:end, 1:end, 1:end])
-        ws[] = arrowlength * vec(spins[3, 1:end, 1:end, 1:end, 1:end])
+        vecs[] = GLMakie.Vec3f.(vec(sys))
         sleep(1/framerate)
     end
 end
@@ -375,42 +328,33 @@ in an interactive window.
 function live_langevin_integration(
     sys::SpinSystem, steps_per_frame, Δt, kT;
     linecolor=:grey, arrowcolor=:red, linewidth=0.1, arrowsize=0.2,
-    arrowlength=1.0, α=0.1, kwargs...
+    arrowlength=0.2, α=0.1, framerate=30, kwargs...
 )
-    sites = reinterpret(reshape, Float64, collect(sys.lattice))
-    spins = 0.2 .* reinterpret(reshape, Float64, sys.sites)
+    fig, ax = _setup_scene()
+    pts = GLMakie.Point3f.(vec(sys.lattice))
+    vecs = GLMakie.Observable(GLMakie.Vec3f.(vec(sys)))
     
-    xs = vec(sites[1, 1:end, 1:end, 1:end, 1:end])
-    ys = vec(sites[2, 1:end, 1:end, 1:end, 1:end])
-    zs = vec(sites[3, 1:end, 1:end, 1:end, 1:end])
-    us = GLMakie.Node(arrowlength * vec(spins[1, 1:end, 1:end, 1:end, 1:end]))
-    vs = GLMakie.Node(arrowlength * vec(spins[2, 1:end, 1:end, 1:end, 1:end]))
-    ws = GLMakie.Node(arrowlength * vec(spins[3, 1:end, 1:end, 1:end, 1:end]))
-    fig, ax, plot = GLMakie.arrows(
-        xs, ys, zs, us, vs, ws;
+    GLMakie.arrows!(
+        ax, pts, vecs;
         linecolor=linecolor, arrowcolor=arrowcolor, linewidth=linewidth, arrowsize=arrowsize,
-        show_axis=false, kwargs...    
+        lengthscale=arrowlength, kwargs...    
     )
     display(fig)
 
-    framerate = 30
     integrator = LangevinHeunP(sys, kT, α)
 
     while true
         for step in 1:steps_per_frame
             evolve!(integrator, Δt)
         end
-        spins = 0.2 .* reinterpret(reshape, Float64, sys.sites)
-        us[] = arrowlength * vec(spins[1, 1:end, 1:end, 1:end, 1:end])
-        vs[] = arrowlength * vec(spins[2, 1:end, 1:end, 1:end, 1:end])
-        ws[] = arrowlength * vec(spins[3, 1:end, 1:end, 1:end, 1:end])
+        vecs[] = GLMakie.Vec3f.(vec(sys))
         sleep(1/framerate)
     end
 end
 
 "Plots slices of a 3D structure factor. Input array should have shape [3, Lx, Ly, Lz, T]"
 function plot_3d_structure_factor(sfactor::Array{Float64, 5}, iz)
-    fig, ax = _setup_scene()
+    fig, ax = _setup_scene(; show_axis=true)
 
     Sdim, Lx, Ly, Lz, T = size(sfactor)
     # Average over Sxx, Syy, Szz - in future give controls to user
