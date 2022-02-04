5df5c814679529ad311a502be813fa36a87cce07
Matt Wilson <mattwilson@pop-os.localdomain>
2022-02-03 19:15:20 -0700

    Add general feedback optimization for REMC and fix alpha setting

--- examples/REMC_Ising2D.jl
+++ examples/REMC_Ising2D.jl
@@ -10,15 +10,27 @@ kT_min = 0.5
 kT_max = 5.0
 
 # Geometric kT schedule
-#kT = kT_min * (kT_max/kT_min) ^ ((rank)/(N_ranks-1))
+kT = kT_min * (kT_max/kT_min) ^ ((rank)/(N_ranks-1))
 
 # Linear kT schedule
-kT = collect(range(kT_min, kT_max, length=N_ranks))[rank+1]
+#kT = collect(range(kT_min, kT_max, length=N_ranks))[rank+1]
+
+#=
+# Linear schedule for external field strength
+H_min = -4.0
+H_max =  4.0
+H = collect(range(H_min, H_max, length=N_ranks))[rank+1] / Sunny.BOHR_MAGNETON
+
+# Fixed temperature
+kT = 2.5
+=#
 
 # Nearest-neighbor ferromagnetic Ising (z-dir) interactions
 J = -1.0
 FM = exchange(diagm([J, J, J]), Bond(1, 1, [1, 0, 0]))
 interactions = [FM]
+#H_ext = external_field([0, 0, H])
+#interactions = [FM, H_ext]
 
 # SC lattice -- extend lattice vector in z-direction to emulate 2D
 lvecs = Sunny.lattice_vectors(1.0, 1.0, 2.0, 90, 90 ,90)
@@ -31,17 +43,33 @@ system = SpinSystem(crystal, interactions, extent, [SiteInfo(1,1,1)])
 randflips!(system)
 
 # Make replica for REMC
-α = kT
+α = 1/kT
 replica = Replica(IsingSampler(system, kT, 1), α)
 
+# Define how the setting of α's changes
+#  the sampling distribution or the Hamiltonian
+function set_α!(replica::Replica, α::Float64)
+    replica.α = α
+    set_temp!(replica.sampler, 1/α)
+end
+
+# Run feedback-optimized replica exchange
+α_opt = run_FBO!(
+    replica,
+    set_α!;
+    max_mcs = 100_000,
+    update_interval = 20_000
+)
+
+#=
 # Run Replica Exchange MC (as PT)
 run_REMC!(
     replica;
-    therm_mcs=5000, 
-    measure_interval=50, 
-    rex_interval=10, 
-    max_mcs=100_000, 
-    bin_size=1.0, 
+    therm_mcs=5000,
+    measure_interval=50,
+    rex_interval=10,
+    max_mcs=100_000,
+    bin_size=1.0,
     print_hist=true
 )
-
+=#
--- src/ReplicaExchangeMC.jl
+++ src/ReplicaExchangeMC.jl
@@ -4,7 +4,7 @@
 Replica type for replica exchange Monte Carlo. A Sunny sampler type must
 be provided during construction.
 """
-mutable struct Replica{AS<:AbstractSampler}
+mutable struct Replica{AS <: AbstractSampler}
     rank::Int64             # MPI rank of sampler is ∈ [0, N_ranks-1]
     N_ranks::Int64          # Total number of MPI ranks (samplers, replicas) 
     N_rex::Int64            # Count of total number of attempted replica exchanges
@@ -33,7 +33,7 @@ end
 Constructor which initializes MPI communicator and sets sampler. -> Use on
 single communicator with no groups for now
 """
-function Replica(sampler::AS, α::Float64) where {AS<:AbstractSampler}
+function Replica(sampler::AS, α::Float64) where {AS <: AbstractSampler}
     # MPI must be initialized by user after importing MPI
     if !MPI.Initialized()
         println("""MPI not initialized. Please add "MPI.Init()" at start of script""")
@@ -217,6 +217,196 @@ function xyz_to_file(sys::SpinSystem, output::IOStream)
     return nothing
 end
 
+""" 
+Update α schedule using feedback-optimization scheme from
+Katzgraber et. al. (https://arxiv.org/pdf/cond-mat/0602085v3.pdf).
+
+Note: use f(θ) that goes from θ_max -> θ_min instead of original paper
+Note: use (k-1)/(M-1) as target for integral in Eq. 11 instead of k/M 
+
+Integration uses dummy variable for θ = 1/α (e.g. θ ↔ kT)    
+"""
+function feedback_update!(replica::Replica, set_α!::F; w::Float64=0.0, dθ′::Float64=1e-5) where {F <: Function}
+    # Storage for updated α
+    α_new = [replica.α]
+
+    # Replica flow
+    N_labeled = (replica.N_up + replica.N_down)
+    f = ((N_labeled > 0) ? replica.N_down/N_labeled : 0.0)
+
+    # Gather θ and replica flow 'f' from all replicas
+    all_θ_and_f = MPI.Gather([1/replica.α, f], 0, MPI.COMM_WORLD)
+
+    if replica.rank == 0
+        all_θ_and_f = reshape(all_θ_and_f, :, replica.N_ranks)
+        θ = all_θ_and_f[1, :]
+        f = all_θ_and_f[2, :]
+
+        M = replica.N_ranks
+        L = collect(range(0, 1.0, length=M))
+
+        # Use 'smoothed replica flow' from Hamze et. al. (https://arxiv.org/pdf/1004.2840.pdf)
+        fs = (1-w).*f .+ (w .* L)
+
+        # Use crude linear approximation for df/dθ
+        Δf = [ ((fs[i+1]-fs[i]) < 0) ? 0 : abs(fs[i+1]-fs[i]) for i in 1:M-1 ]
+        Δθ = abs.(θ[2:end] .- θ[1:end-1])
+
+        # Calculated normalized density for adjusted θ's
+        η = sqrt.( (Δf ./ Δθ) ./ Δθ )
+        C = sum(η .* Δθ)
+        η ./= C
+
+        # New θ's (and α's) have fixed end points
+        θ_new = zeros(M)
+        θ_new[1] = θ[1]
+        θ_new[M] = θ[M]
+
+        θ′= θ[1]
+        cηf = 0.0
+        pos = 2
+
+        # find new temperatures using Eq. 11 from Katzgraber et. al.
+        for i in 1:M-1
+            for j in 1:Int(round(Δθ[i]/dθ′))
+                cηf += η[i]*dθ′
+                θ′ += dθ′
+
+                if (pos < M) && (cηf >= L[pos])
+                    θ_new[pos] = θ′
+                    pos += 1
+                end
+            end
+        end
+        # Send out updated α to all replicas
+        MPI.Scatter!(MPI.UBuffer(1 ./ θ_new, 1), α_new, 0, MPI.COMM_WORLD)
+    else
+        MPI.Scatter!(nothing, α_new, 0, MPI.COMM_WORLD)
+    end
+
+    # Assign new value of α and set in replica sampler
+    # -> corresponds to update of kT or other Hamiltonian parameter
+    set_α!(replica, α_new[1])
+
+    return nothing
+end
+
+"""
+"""
+function run_FBO!(
+    replica::Replica,
+    set_α!::F;
+    therm_mcs::Int64=1000,
+    rex_interval::Int64=1,
+    max_mcs::Int64=500_000,
+    update_interval::Int64=100_000,
+    w::Float64=0.0,
+    dθ′::Float64=1e-5,
+    print_ranks_trajectory::Vector{Int64}=Int64[],
+    trajectory_interval::Int64=500_000
+) where {F <: Function}
+
+    # Initialize energy and equilibrate replica to it's distribution
+    reset_running_energy!(replica.sampler)
+    reset_running_mag!(replica.sampler)
+
+    thermalize!(replica.sampler, therm_mcs)
+
+    replica.sampler.nsweeps = 1
+
+    rex_accepts = zeros(Int64, 2)
+    N_updates = 0
+
+    # Start feedback optimization simulation
+    for mcs in 1:max_mcs
+        sample!(replica.sampler)
+
+        if (replica.rank == 0) && (mcs % 1000 == 0)
+            println("FBOPT ", mcs, " mcs")
+        end
+
+        # Attempt replica exchange
+        if mcs % rex_interval == 0
+            if replica_exchange!(replica)
+                rex_accepts[replica.rex_dir] += 1
+
+                # Exchange labels
+                label_exchange!(replica, replica.nn_ranks[replica.rex_dir])
+            end
+
+            # Record replica flow  
+            if replica.nn_ranks[replica.rex_dir] != -1
+                if replica.label < 0
+                    replica.N_down += 1
+                elseif replica.label < replica.N_ranks+1
+                    replica.N_up += 1
+                end
+            end
+
+            # Print out replica exchange timeseries -- inefficient IO
+            if (replica.N_rex % trajectory_interval == 0) && (abs(replica.label)-1 in print_ranks_trajectory)
+                if replica.label <= replica.N_ranks
+                    fname = @sprintf("P%03d_FBO_trajectory_%03d.dat", abs(replica.label)-1, N_updates)
+                    output = open(fname, "a")
+                    println(output, replica.rank)
+                    close(output)
+                end
+            end
+
+            # Alternate up/down pairs of replicas for exchanges
+            replica.rex_dir = 3 - replica.rex_dir
+        end
+
+        if mcs % update_interval == 0
+            # Replica flow 'f' and smoothed replica flow 'fs'
+            N_labeled = (replica.N_up + replica.N_down)
+            f = ((N_labeled > 0) ? replica.N_down/N_labeled : 0.0)
+            fs = (1-w)*f + w*replica.rank/(replica.N_ranks-1)
+
+            # Acceptance rate between replicas i and i+1
+            A = ((replica.rank == replica.N_ranks-1) ? 0.0 : rex_accepts[2]/replica.N_rex)
+
+            # Print out α, replica flow, and acceptance rates for each update
+            fname = @sprintf("FBO_%03d.dat", N_updates)
+            gather_print(
+                replica,
+                [replica.α, fs, A],
+                fname
+            )
+
+            # Perform feedback optimization for control parameters
+            feedback_update!(replica, set_α!; w=w, dθ′=dθ′)
+
+            N_updates += 1
+            rex_accepts[1] = rex_accepts[2] = 0
+            reset_stats!(replica)
+        end
+    end
+
+    rex_accepts[1] = rex_accepts[2] = 0
+    reset_stats!(replica)
+
+    # Return a vector of optimized α's
+    return MPI.Allgather([replica.α], MPI.COMM_WORLD)
+end
+
+"""
+"""
+function reset_stats!(replica::Replica)
+    replica.N_up = replica.N_down = 0
+    replica.N_rex  = 0
+
+    # Reset label
+    if replica.rank == 0
+        replica.label = 1
+    elseif replica.rank == replica.N_ranks-1
+        replica.label = -replica.N_ranks
+    else
+        replica.label = replica.N_ranks + replica.rank+1
+    end
+    return nothing
+end
+
 """
 Start a parallel tempering (PT) simulation that saves measured averages,
 histograms, minimal energies, and configurations.
@@ -297,7 +487,6 @@ function run_REMC!(
 
         # Attempt replica exchange
         if mcs % rex_interval == 0 
-            # Accept exchange
             if replica_exchange!(replica)
                 rex_accepts[replica.rex_dir] += 1
 
@@ -333,7 +522,7 @@ function run_REMC!(
         # Measurements
         if mcs % measure_interval == 0
             E = running_energy(replica.sampler)
-            m = norm(running_mag(replica.sampler))
+            m = (running_mag(replica.sampler))[3]
 
             # New minimum energy measured
             if E < Emin
