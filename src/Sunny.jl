649aa9d5d0f7eddf58b826be6bab35cfd15c45d7
Kipton Barros <kbarros@gmail.com>
2022-07-01 17:18:05 -0600

    Export offline_viewers

--- src/Sunny.jl
+++ src/Sunny.jl
@@ -100,7 +100,7 @@ include("WangLandau/WangLandau.jl")
 export WangLandau, spherical_cap_update, init_bounded!, run!
 
 include("SunnyGfx/SunnyGfx.jl")
-export view_crystal
+export view_crystal, offline_viewers
 
 function __init__()
     @require GLMakie="e9467ef8-e4e7-5192-8a1a-b1aee30e663a" begin
