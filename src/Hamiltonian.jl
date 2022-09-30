SunnySuite/Sunny.jl

commit 08c783fd26fce30d73480597a373302b26b41f24

Fix wrapping tests (#41)
Be more accurate in determining whether the SpinSystem dimensions are sufficiently large for a given set of interactions.

src/Hamiltonian.jl
@@ -4,37 +4,41 @@
 function validate_and_clean_interactions(ints::Vector{<:AbstractInteraction}, crystal::Crystal, latsize::Vector{Int64})
     # Validate all interactions
     for int in ints
+        int_str = repr("text/plain", int)
+
         if isa(int, QuadraticInteraction)
             b = int.bond
 
             # Verify that both basis sites indexed actually exist
             if !(1 <= b.i <= nbasis(crystal)) || !(1 <= b.j <= nbasis(crystal))
-                error("Provided interaction $(repr(MIME("text/plain"), int)) indexes a non-existent basis site.")
+                error("Provided interaction $int_str indexes a non-existent basis site.")
             end
 
             # Verify that the interactions are symmetry-consistent
             if !is_coupling_valid(crystal, b, int.J)
-                println("Symmetry-violating interaction: $(repr(MIME("text/plain"), int)).")
+                println("Symmetry-violating interaction: $int_str.")
                 println("Allowed exchange for this bond:")
                 print_allowed_coupling(crystal, b; prefix="    ")
                 println("Use `print_bond(crystal, $b)` for more information.")
                 error("Interaction violates symmetry.")
             end
 
-            # Verify that no bond wraps the entire system
+            # Verify that no bond is long enough to wrap the entire system
             bs = all_symmetry_related_bonds(crystal, b)
-            wraps = any(bs) do b
-                any(abs.(b.n) .>= latsize)
-            end
-            if wraps
-                println("Distance-violating interaction: $int.")
-                error("Interaction wraps system.")
+            for b′ in bs
+                coeffs = crystal.lat_vecs \ displacement(crystal, b′)
+                for i = 1:3
+                    if abs(coeffs[i]) >= latsize[i]/2 - 1e-10
+                        println("Interaction $int_str is too long; consider extending system along dimension $i.")
+                        error("Interaction wraps system.")
+                    end
+                end
             end
         elseif isa(int, QuadraticAnisotropy)
             site = int.site
             b = Bond(site, site, [0, 0, 0])
             if !is_coupling_valid(crystal, b, int.J)
-                println("Symmetry-violating anisotropy: $(repr(MIME("text/plain"), int)).")
+                println("Symmetry-violating anisotropy: $int_str.")
                 println("Allowed single-ion anisotropy for this atom:")
                 print_allowed_coupling(crystal, b; prefix="    ")
                 println("Use `print_bond(crystal, Bond($site, $site, [0,0,0])` for more information.")
@@ -45,7 +49,7 @@ function validate_and_clean_interactions(ints::Vector{<:AbstractInteraction}, cr
             b = Bond(site, site, [0,0,0])
             N = size(Λ)[1]
             if !is_anisotropy_valid(crystal, site, int.Λ)
-                println("Symmetry-violating anisotropy: $(repr(MIME("text/plain"), int)).")
+                println("Symmetry-violating anisotropy: $int_str.")
                 println("Allowed SU(N) single-ion anisotropy for this atom:")
                 print_allowed_anisotropy(crystal, site)
                 error("Specified SU($N) anisotropy either violates symmetry or is of incorrect dimension.")

test/test_lattice.jl
@@ -1,2 +1,43 @@
-@testitem "Lattice Type" begin
+@testitem "Lattice construction" begin
+
+    @testset "Wrapping" begin
+        latvecs = lattice_vectors(1, 1, 2, 90, 90, 90)
+        basis_vectors = [[0, 0, 0]]
+        cryst = Crystal(latvecs, basis_vectors)
+        ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
+        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (3, 2, 4))
+        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 3, 4))
+        @test SpinSystem(cryst, ints, (3, 3, 4)) isa SpinSystem
+
+        latvecs = lattice_vectors(0.5, 1.0, 2, 90, 90, 90)
+        basis_vectors = [[0, 0, 0]]
+        cryst = Crystal(latvecs, basis_vectors)
+        ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
+        @test SpinSystem(cryst, ints, (3, 1, 4)) isa SpinSystem
+        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 3, 4))
+
+        # Xiaojian's original test case
+        latvecs = lattice_vectors(1, 1, 2, 90, 90, 90)
+        basis_vectors = [[0, 0, 0], [0.5, 0.9, 0]]
+        cryst = Crystal(latvecs, basis_vectors)
+        ints = [heisenberg(1.0, Bond(1, 2, [0, -1, 0]))]
+        @test SpinSystem(cryst, ints, (2, 1, 4)) isa SpinSystem
+        ints = [heisenberg(1.0, Bond(1, 2, [0, 0, 0]))]
+        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 1, 4))
+
+        latvecs = lattice_vectors(1, 1, 2, 90, 90, 120)
+        basis_vectors = [[0, 0, 0]]
+        cryst = Crystal(latvecs, basis_vectors)
+        ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
+        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (3, 2, 4))
+        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 3, 4))
+        @test SpinSystem(cryst, ints, (3, 3, 4)) isa SpinSystem
+
+        latvecs = lattice_vectors(0.5, 1.0, 2, 90, 90, 120)
+        basis_vectors = [[0, 0, 0]]
+        cryst = Crystal(latvecs, basis_vectors)
+        ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
+        @test SpinSystem(cryst, ints, (3, 1, 4)) isa SpinSystem
+        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 3, 4))
+    end
 end

test/test_shared.jl
@@ -1,3 +1,9 @@
+# Currently each @testitem must run in isolation. To share common setup code for
+# tests, the recommended pattern is to `include()` a file such as this one. See:
+# https://discourse.julialang.org/t/prerelease-of-new-testing-framework-and-test-run-ui-in-vs-code/86355/37
+# In the future, TestItemRunner may support a better pattern:
+# https://github.com/julia-vscode/TestItemRunner.jl/issues/11
+
 using Random
 using LinearAlgebra
 

test/test_spin_scaling.jl
@@ -28,7 +28,7 @@ function make_test_system_lld(; spin_rescaling=1.0)
     quartic_interactions = [quartic_anisotropy(Jquar, i, "quartic") for i ∈ 1:4]
 
     interactions_all = vcat(exchange_interactions..., quartic_interactions...) 
-    dims = (2,2,2)
+    dims = (3,3,3)
 
     return SpinSystem(cryst,
                       interactions_all,
@@ -48,7 +48,7 @@ function make_test_system_gsd(; spin_rescaling=1.0, N=2)
     S = Sunny.gen_spin_ops(N)
     quartic_sun = SUN_anisotropy(-S[3]^4, 1, "quartic") 
 
-    dims = (2,2,2)
+    dims = (3,3,3)
     interactions_all = vcat(exchange_interactions..., quartic_sun) 
 
     return SpinSystem(cryst,
@@ -232,7 +232,7 @@ so results with different spin magnitudes can be compared directly.
 function generate_scaled_quadratic_trajectory(spin_rescaling, Δt; N=0, dur=10.0)
     rng = Random.MersenneTwister(111)
     cryst = Sunny.cubic_crystal()
-    dims = (4,4,2)
+    dims = (4,4,3)
     interactions = [
         heisenberg(1.0, Bond(1,1,[1,0,0])),
         dipole_dipole()

commit 61bb5264d5136cf6cbdc5c35222d5ba3caafd8cb

Remove uses of Logging macros (#45)
Note that `error()` throws an exception, whereas `@error` does not.
Convert all instances to the former.

For consistency with the rest of the codebase, remove usages of
`@warn`. Eventually we may want to replace all `println()` warning
statements with `@warn`.

This PR also downgrades the error introduced in
https://github.com/SunnySuite/Sunny.jl/pull/41 to just a warning.

src/Hamiltonian.jl
@@ -29,8 +29,7 @@ function validate_and_clean_interactions(ints::Vector{<:AbstractInteraction}, cr
                 coeffs = crystal.lat_vecs \ displacement(crystal, b′)
                 for i = 1:3
                     if abs(coeffs[i]) >= latsize[i]/2 - 1e-10
-                        println("Interaction $int_str is too long; consider extending system along dimension $i.")
-                        error("Interaction wraps system.")
+                        println("Warning: Interaction $int_str wraps the system along dimension $i.")
                     end
                 end
             end
@@ -112,9 +111,9 @@ function propagate_sun_anisos(crystal::Crystal, anisos::Vector{SUNAnisotropy}, N
 
         for (sym_atom, sym_Λ) in zip(sym_bs, sym_Λs)
             if sym_atom in specified_atoms
-                @error "Provided two SU($N) anisotropies for symmetry equivalent sites."
+                error("Provided two SU($N) anisotropies for symmetry equivalent sites.")
             elseif size(sym_Λ)[1] != N
-                @error "Provided an SU($(size(sym_Λ)[1])) anisotropy for an SU($N) model!"
+                error("Provided an SU($(size(sym_Λ)[1])) anisotropy for an SU($N) model!")
             else
                 push!(specified_atoms, sym_atom)
             end
@@ -140,7 +139,7 @@ function merge_upconvert_anisos(anisos::Vector{<:AbstractAnisotropy}, crystal::C
     # Convert to backend types if in LL mode.
     if N == 0
         if length(sun_anisos) != 0
-            @error "Given a SU(N) anisotropy but running in classic Landau-Lifshitz mode."
+            error("Given a SU(N) anisotropy but running in classic Landau-Lifshitz mode.")
         end
 
         quadratic_aniso = merge(quadratic_anisos)
@@ -151,7 +150,7 @@ function merge_upconvert_anisos(anisos::Vector{<:AbstractAnisotropy}, crystal::C
 
     # Throw error if given non-SU(N) anisotropy and in SU(N) mode 
     if length(quadratic_anisos) != 0 || length(quartic_anisos) != 0
-        @error "Given a Landau-Lifshitz-type anisotropy, but running in SU(N) mode."
+        error("Given a Landau-Lifshitz-type anisotropy, but running in SU(N) mode.")
     end
 
     # Propagate SU(N) anisotropies to symmetry equivalent sites
@@ -228,7 +227,7 @@ function HamiltonianCPU(ints::Vector{<:AbstractInteraction}, crystal::Crystal,
             push!(anisos, int)
         elseif isa(int, DipoleDipole)
             if !isnothing(dipole_int)
-                @warn "Provided multiple dipole interactions. Only using last one."
+                println("Warning: Provided multiple dipole interactions. Only using last one.")
             end
             dipole_int = DipoleFourierCPU(int, crystal, latsize, site_infos; μB=μB, μ0=μ0)
         else

src/Integrators.jl
@@ -83,7 +83,7 @@ mutable struct SphericalMidpoint <: Integrator
 end
 
 function SphericalMidpoint(sys::SpinSystem{N}; atol=1e-12) where N
-    @error "SphericalMidpoint integrator is not available for SU(N) systems. Use ImplicitMidpoint integrator."
+    error("SphericalMidpoint integrator is not available for SU(N) systems. Use ImplicitMidpoint integrator.")
     nothing
 end
 
@@ -172,7 +172,7 @@ function SchrodingerMidpoint(sys::SpinSystem{N}; atol=1e-14) where N
 end
 
 function SchrodingerMidpoint(sys::SpinSystem{0}; atol=1e-14)
-    @error "SchrodingerMidpoint integration is only available for SU(N) systems. Use ImplicitMidpoint integrator."
+    error("SchrodingerMidpoint integration is only available for SU(N) systems. Use ImplicitMidpoint integrator.")
     nothing
 end
 

src/Interactions.jl
@@ -104,7 +104,9 @@ function SiteInfo(site::Int; N=0, g=2*I(3), spin_rescaling=1.0, ff_elem=nothing,
 
     # Make sure a valid element is given if a g_lande value is given. 
     if isnothing(ff_elem) && !isnothing(ff_lande)
-        @warn "When creating a SiteInfo, you must provide valid `ff_elem` if you are also assigning a value to `ff_lande`. No form factor corrections will be applied."
+        println("""Warning: When creating a SiteInfo, you must provide valid `ff_elem` if you
+                   are also assigning a value to `ff_lande`. No form factor corrections will be
+                   applied.""")
     end
 
     # Read all relevant form factor data if an element name is provided

src/Symmetry/Crystal.jl
@@ -370,8 +370,8 @@ function crystal_from_symops(lat_vecs::Mat3, positions::Vector{Vec3}, types::Vec
     end
 
     if !is_subgroup
-        println("WARNING: User provided symmetry operation could not be inferred by Spglib,")
-        println("which likely indicates a non-conventional unit cell.")
+        println("""Warning: User provided symmetry operation could not be inferred by Spglib,
+                   which likely indicates a non-conventional unit cell.""")
     end
 
     # If the inferred symops match the provided ones, then we use the inferred
@@ -424,7 +424,7 @@ function subcrystal(cryst::Crystal, classes::Vararg{Int, N}) where {N}
     new_sitesyms = cryst.sitesyms[idxs]
 
     if idxs != 1:maximum(idxs)
-        println("WARNING, atoms are being renumbered.")
+        println("Warning: atoms are being renumbered.")
     end
 
     ret = Crystal(cryst.lat_vecs, new_positions, new_types, new_classes, new_sitesyms,

src/Systems.jl
@@ -138,7 +138,7 @@ function _propagate_site_info(crystal::Crystal, site_infos::Vector{SiteInfo})
     for siteinfo in site_infos
         (; site, N, g, spin_rescaling, ff_params) = siteinfo
         if N != maxN
-            @warn "Up-converting N=$N -> N=$maxN on site $(site)!"
+            println("Warning: Up-converting N=$N -> N=$maxN on site $(site)!")
         end
         (sym_bs, sym_gs) = all_symmetry_related_couplings(crystal, Bond(site, site, [0,0,0]), g)
         for (sym_b, sym_g) in zip(sym_bs, sym_gs)
@@ -146,7 +146,7 @@ function _propagate_site_info(crystal::Crystal, site_infos::Vector{SiteInfo})
             if sym_atom in specified_atoms
                 # Perhaps this should only throw if two _conflicting_ SiteInfo are passed?
                 # Then propagate_site_info can be the identity on an already-filled list.
-                @error "Provided two `SiteInfo` which describe symmetry-equivalent sites!"
+                error("Provided two `SiteInfo` which describe symmetry-equivalent sites!")
             else
                 push!(specified_atoms, sym_atom)
             end
@@ -159,7 +159,9 @@ function _propagate_site_info(crystal::Crystal, site_infos::Vector{SiteInfo})
     with_ff = filter(si -> !isnothing(si.ff_params), all_site_infos)
     if length(with_ff) > 0
         if length(with_ff) != length(all_site_infos)
-            @error "Form factor calculations require that the magnetic ion be specified for all unique lattice sites. Please provide a SiteInfo with an explicit ff_elem for all unique sites or for none at all"
+            error("""Form factor calculations require that the magnetic ion be specified for
+                     all unique lattice sites. Please provide a SiteInfo with an explicit
+                     ff_elem for all unique sites or for none at all.""")
         end
     end
 

test/test_lattice.jl
@@ -1,43 +1,45 @@
 @testitem "Lattice construction" begin
+    # TODO: Send all warnings to stderr and reenable @test_warn once
+    # TestItemRunner supports it.
+    # https://github.com/julia-vscode/TestItemRunner.jl/issues/25
 
-    @testset "Wrapping" begin
-        latvecs = lattice_vectors(1, 1, 2, 90, 90, 90)
-        basis_vectors = [[0, 0, 0]]
-        cryst = Crystal(latvecs, basis_vectors)
-        ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
-        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (3, 2, 4))
-        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 3, 4))
-        @test SpinSystem(cryst, ints, (3, 3, 4)) isa SpinSystem
+    latvecs = lattice_vectors(1, 1, 2, 90, 90, 90)
+    basis_vectors = [[0, 0, 0]]
+    cryst = Crystal(latvecs, basis_vectors)
+    ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
+    # @test_warn "Interaction" SpinSystem(cryst, ints, (3, 2, 4))
+    # @test_warn "Interaction" SpinSystem(cryst, ints, (2, 3, 4))
+    @test SpinSystem(cryst, ints, (3, 3, 4)) isa SpinSystem
 
-        latvecs = lattice_vectors(0.5, 1.0, 2, 90, 90, 90)
-        basis_vectors = [[0, 0, 0]]
-        cryst = Crystal(latvecs, basis_vectors)
-        ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
-        @test SpinSystem(cryst, ints, (3, 1, 4)) isa SpinSystem
-        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 3, 4))
+    latvecs = lattice_vectors(0.5, 1.0, 2, 90, 90, 90)
+    basis_vectors = [[0, 0, 0]]
+    cryst = Crystal(latvecs, basis_vectors)
+    ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
+    @test SpinSystem(cryst, ints, (3, 1, 4)) isa SpinSystem
+    # @test_warn "Interaction" SpinSystem(cryst, ints, (2, 3, 4))
 
-        # Xiaojian's original test case
-        latvecs = lattice_vectors(1, 1, 2, 90, 90, 90)
-        basis_vectors = [[0, 0, 0], [0.5, 0.9, 0]]
-        cryst = Crystal(latvecs, basis_vectors)
-        ints = [heisenberg(1.0, Bond(1, 2, [0, -1, 0]))]
-        @test SpinSystem(cryst, ints, (2, 1, 4)) isa SpinSystem
-        ints = [heisenberg(1.0, Bond(1, 2, [0, 0, 0]))]
-        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 1, 4))
+    # Xiaojian's original test case
+    latvecs = lattice_vectors(1, 1, 2, 90, 90, 90)
+    basis_vectors = [[0, 0, 0], [0.5, 0.9, 0]]
+    cryst = Crystal(latvecs, basis_vectors)
+    ints = [heisenberg(1.0, Bond(1, 2, [0, -1, 0]))]
+    @test SpinSystem(cryst, ints, (2, 1, 4)) isa SpinSystem
+    ints = [heisenberg(1.0, Bond(1, 2, [0, 0, 0]))]
+    # @test_warn "Interaction" SpinSystem(cryst, ints, (2, 1, 4))
 
-        latvecs = lattice_vectors(1, 1, 2, 90, 90, 120)
-        basis_vectors = [[0, 0, 0]]
-        cryst = Crystal(latvecs, basis_vectors)
-        ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
-        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (3, 2, 4))
-        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 3, 4))
-        @test SpinSystem(cryst, ints, (3, 3, 4)) isa SpinSystem
+    latvecs = lattice_vectors(1, 1, 2, 90, 90, 120)
+    basis_vectors = [[0, 0, 0]]
+    cryst = Crystal(latvecs, basis_vectors)
+    ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
+    # @test_warn "Interaction" SpinSystem(cryst, ints, (3, 2, 4))
+    # @test_warn "Interaction" SpinSystem(cryst, ints, (2, 3, 4))
+    @test SpinSystem(cryst, ints, (3, 3, 4)) isa SpinSystem
 
-        latvecs = lattice_vectors(0.5, 1.0, 2, 90, 90, 120)
-        basis_vectors = [[0, 0, 0]]
-        cryst = Crystal(latvecs, basis_vectors)
-        ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
-        @test SpinSystem(cryst, ints, (3, 1, 4)) isa SpinSystem
-        @test_throws ErrorException("Interaction wraps system.") SpinSystem(cryst, ints, (2, 3, 4))
-    end
+    latvecs = lattice_vectors(0.5, 1.0, 2, 90, 90, 120)
+    basis_vectors = [[0, 0, 0]]
+    cryst = Crystal(latvecs, basis_vectors)
+    ints = [heisenberg(1.0, Bond(1, 1, [1, 0, 0]))]
+    @test SpinSystem(cryst, ints, (3, 1, 4)) isa SpinSystem
+    # @test_warn "Interaction" SpinSystem(cryst, ints, (2, 3, 4))
+
 end