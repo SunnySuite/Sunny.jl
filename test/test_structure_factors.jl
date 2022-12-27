e7464894d2c516fc6647c569bf9252afb413d233
ddahlbom <david.dahlbom@gmail.com>
2022-12-27 14:14:54 -0500

    Added sum rule test

--- test/test_structure_factors.jl
+++ test/test_structure_factors.jl
@@ -1,5 +1,49 @@
-@testitem "Structure Factors" begin
+@testitem "Sum Rule" begin
 
-# Write new tests
+    function simple_model_sf(; dims = (4, 4, 4), N=0)
+        J = 1.0
+        cryst = Sunny.fcc_primitive_crystal()
+        interactions = [heisenberg(J, Bond(1, 1, [1, 0, 0]))]
+        spin_rescaling = N == 0 ? 1.0 : 2.0/(N-1)
+        sys = SpinSystem(cryst, interactions, dims, [SiteInfo(1; N, spin_rescaling)])
+        return sys
+    end
+
+    function thermalize_simple_model!(sys; T = 1.0)
+        Δt = 0.05  # Time step for thermalization
+        kT = Sunny.CONSTS_meV.kB * T 
+        λ  = 0.1
+        nsteps = 1000  # Number of steps between MC samples
+        integrator = LangevinHeunP(kT, λ, Δt)
+        for _ ∈ 1:nsteps
+            step!(sys, integrator)
+        end
+    end
     
+    # Trial parameters
+    ωmax = 10.0
+    dims = (4, 4, 4)
+    gfactor = false
+    Ns = [0, 2]
+
+    for N in Ns
+        ops = if N == 2
+            Ss = Sunny.spin_matrices(2)
+            ops = zeros(ComplexF64, 2, 2, 3)
+            for i ∈ 1:3
+                ops[:,:,i] = Ss[i]
+            end 
+            ops
+        else
+            nothing
+        end
+
+        sys = simple_model_sf(; N)
+        sf = StructureFactor(sys; ωmax, gfactor, ops)
+        thermalize_simple_model!(sys)
+        add_trajectory!(sf, sys)
+        intensities = intensity_grid(sf; contraction=Trace(), negative_energies=true)
+
+        @test isapprox(sum(intensities) / *(dims...), 1.0; atol=1e-12)
+    end
 end
