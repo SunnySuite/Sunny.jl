5925fba1d1aebfc82eb19fbe46e0006f0b5f07bc
Kipton Barros <kbarros@gmail.com>
2023-01-29 09:07:37 -0700

    Supercell suggestion
    Implement print_dominant_wavevectors and suggest_magnetic_supercell

--- src/System/System.jl
+++ src/System/System.jl
@@ -243,3 +243,155 @@ function get_coherent_buffers(sys::System, numrequested)
     end
     return sys.coherent_buffers[1:numrequested]
 end
+
+
+wavevec_str(q) = "["*join(number_to_math_string.(q), ", ")*"]"
+
+"""
+    print_dominant_wavevectors(sys::System; nmax=10)
+
+Prints a list of wavevectors in the first Brillouin zone according to their
+contributions to the structure factor weights, accumulated over individual
+sublattices. Coordinates are measured in units of reciprocal lattice vectors.
+These dominant wavevectors may be used as input to
+[`suggest_magnetic_supercell`](@ref). Unlike in [`static_intensities`](@ref),
+here the structure factor weights do not incorporate phase averaging between the
+sublattices.
+"""
+function print_dominant_wavevectors(sys::System{N}; nmax=10) where N
+    s = reinterpret(reshape, Float64, sys.dipoles)
+    V = prod(sys.latsize) # number of spins in sublattice
+    cell_dims = (2,3,4)
+    sk = FFTW.fft(s, cell_dims) / √V
+    Sk = real.(conj.(sk) .* sk)
+    dims = (1,5) # sum over spin index and basis index
+    # In Julia 1.9 this becomes: sum(eachslice(dat; dims)))
+    Sk = dropdims(sum(Sk; dims); dims)
+    @assert sum(Sk) ≈ norm(sys.dipoles)^2
+
+    weight = Sk .* (100 / norm(sys.dipoles)^2)
+    p = sortperm(-weight[:])
+
+    println("Dominant wavevectors for spin sublattices:\n")
+    for (i, m) in enumerate(CartesianIndices(sys.latsize)[p])
+        q = (Tuple(m) .- 1) ./ sys.latsize
+        q = [qi > 1/2 ? qi-1 : qi for qi in q]
+
+        qstr = wavevec_str(q)
+        
+        if weight[m] < 0.01
+            break
+        end
+
+        wstr = @sprintf "%6.2f" weight[m]
+        nspaces = 20
+        spacing = " " ^ max(0, nspaces - length(qstr))
+        print("    $qstr $spacing $wstr%")
+        println(i == 1 ? " weight" : "")
+
+        if i > nmax
+            spacing = " " ^ (nspaces-1)
+            println("    ... $spacing ...")
+            break
+        end
+    end
+end
+
+"""
+    suggest_magnetic_supercell(qs, latsize)
+
+Suggests a magnetic supercell, in units of the crystal lattice vectors, that is
+consistent with periodicity of the wavevectors in `qs`. The wavevectors should
+be commensurate with `latsize` in units of the lattice vectors.
+"""
+function suggest_magnetic_supercell(qs, latsize)
+    foreach(qs) do q
+        m = round.(Int, q .* latsize)
+        if norm(m - q .* latsize) > 1e-12
+            error("Wavevector $q incommensurate with lattice size $latsize")
+        end
+    end
+
+    # All possible periodic offsets
+    nmax = latsize .÷ 2
+    ns = [Vec3(n1, n2, n3) for n1 in -nmax[1]:nmax[1], n2 in -nmax[2]:nmax[2], n3 in -nmax[3]:nmax[3]][:]
+
+    # Filter out elements of ns that are not consistent with q ∈ qs
+    for q in qs
+        ns = filter(ns) do n            
+            # Proposed offset is `r = nᵅ aᵅ` in lattice vectors `aᵅ`. Wave
+            # vector is `k = qᵝ bᵝ` in reciprocal vectors `bᵝ`. Using
+            # orthogonality property `aᵅ⋅bᵝ = 2πδᵅᵝ`, it follows `r⋅k = 2π n⋅q`.
+            # The integer offsets `n` are allowed if `n⋅q` is exactly integer,
+            # which confirms periodicity, `exp(-i r⋅k) = 1`.
+            is_approx_integer(n⋅q; atol=1e-12)
+        end
+    end
+
+    # Naive supercell is the entire lattice
+    best_score = Inf
+    best_A = diagm(collect(latsize))
+
+    # Goodness of supervectors A1, A2, A3. Lower is better.
+    function score(A)
+        (A1, A2, A3) = eachcol(A)
+        # Minimize supercell volume
+        V = (A1 × A2) ⋅ A3
+        # println(A)
+        # println(V)
+        # To split ties, maximize orthogonality
+        c1 = (normalize(A1) × normalize(A2)) ⋅ normalize(A3)
+        # To split ties, maximize alignment with Cartesian system
+        c2 = normalize(A1)[1] + normalize(A2)[2] + normalize(A3)[3]
+        V <= 0 ? Inf : V - 1e-3*c1 - 1e-6c2
+    end
+
+    any_changed = true
+    while any_changed
+        any_changed = false
+
+        # try all possible replacements of individual columns
+        for j in 1:3, n in ns
+            A = copy(best_A)
+            A[:, j] = n
+            if best_score > score(A)
+                best_score = score(A)
+                best_A = A
+                any_changed = true
+            end
+        end
+
+        # try permuting columns with various sign changes
+        (A1, A2, A3) = eachcol(best_A)
+        for A in ([A1 -A3 A2], [A1 A3 -A2],
+                  [-A3 A2 A1], [A3 A2 -A1],
+                  [-A2 A1 A3], [A2 -A1 A3])
+            if best_score > score(A)
+                best_score = score(A)
+                best_A = A
+                any_changed = true
+            end
+        end
+    end
+
+    # # Alternative brute force implementation, for reference
+    # push!(ns, [latsize[1],0,0])
+    # push!(ns, [0,latsize[2],0])
+    # push!(ns, [0,0,latsize[3]])
+    # for A1 in ns, A2 in ns, A3 in ns
+    #     A = [A1 A2 A3]
+    #     if best_score > score(A)
+    #         best_score = score(A)
+    #         best_A = A
+    #     end
+    # end
+
+
+    qstrs = join(map(wavevec_str, qs), ", ")
+
+    println("Suggested magnetic supercell in multiples of lattice vectors:")
+    println()
+    println("    A = $best_A")
+    println()
+    println("for wavevectors [$qstrs].")
+end
