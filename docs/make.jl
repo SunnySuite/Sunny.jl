f6ed1201165853869128304fdf7740c3df5f7be5
ddahlbom <david.dahlbom@gmail.com>
2023-02-01 01:21:44 -0500

    Literate added to make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,21 @@
 push!(LOAD_PATH, "../src/")
 
-using Documenter, Sunny
-# Need to import libraries which expose functions hidden behind Requires.jl
+using Literate, Documenter, Sunny
 # using GLMakie
+
+FEI2 = joinpath(@__DIR__, "..", "examples", "fei2_tutorial.jl")
+OUTPUT = joinpath(@__DIR__, "src", "literate_build")
+
+Literate.markdown(FEI2, OUTPUT; execute=true, documenter=true)
 
 makedocs(
     sitename="Sunny documentation",
     pages = [
         "Overview" => "index.md",
         "Quick Start" => "quick-start.md",
+        "Examples" => Any[
+            "literate_build/fei2_tutorial.md",
+        ],
         "Library API" => "library.md",
         "Structure Factor Calculations" => "structure-factor.md",
         "Version History" => "versions.md",
