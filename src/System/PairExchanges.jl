7ab98a0ed17fff0b8e2f625faa7f542029722852
Kipton Barros <kbarros@gmail.com>
2023-02-12 13:12:23 -0700

    Progress

--- src/System/PairExchanges.jl
+++ src/System/PairExchanges.jl
@@ -1,3 +1,12 @@
+function verify_bond(cryst::Crystal, bond::Bond)
+    # Verify bond indices
+    if bond.i == bond.j && iszero(bond.n)
+        error("Bond must connect different atoms.")
+    end
+    (1 <= bond.i <= nbasis(cryst)) || error("Atom index $(bond.i) is out of range.")
+    (1 <= bond.j <= nbasis(cryst)) || error("Atom index $(bond.j) is out of range.")
+end
+
 """
     set_biquadratic!(sys::System, J, bond::Bond)
 
@@ -22,23 +31,12 @@ function set_biquadratic!(sys::System{N}, J, bond::Bond) where N
         return
     end
 
-    # Biquadratic interactions not yet supported in SU(N) mode
-    if !iszero(J) && sys.mode==:SUN
-        error("Biquadratic interactions not yet supported in SU(N) mode.")
-    end
-
-    if !is_homogeneous(sys)
-        error("Use `set_biquadratic!` for inhomogeneous systems.")
-    end
+    sys.mode==:SUN && error("Biquadratic interactions not yet supported in SU(N) mode.")
+    verify_bond(sys.crystal, bond)
+    is_homogeneous(sys) || error("Cannot symmetry-propagate interactions for an inhomogeneous system.")
 
     ints = interactions(sys)
 
-    # Verify bond indices
-    if bond.i == bond.j && iszero(bond.n)
-        error("Bond must connect different sites.")
-    end
-    (1 <= bond.i <= nbasis(sys.crystal)) || error("Atom index $(bond.i) is out of range.")
-    (1 <= bond.j <= nbasis(sys.crystal)) || error("Atom index $(bond.j) is out of range.")
 
     # Print a warning if an interaction already exists for bond
     if any(x -> x.bond == bond, ints[bond.i].biquad)
@@ -107,20 +105,14 @@ function set_exchange!(sys::System{N}, J, bond::Bond) where N
         return
     end
 
-    if !is_homogeneous(sys)
-        error("Use `set_exchange_at!` for inhomogeneous systems.")
-    end
-    ints = interactions(sys)
+    verify_bond(sys.crystal, bond)
+    is_homogeneous(sys) || error("Cannot symmetry-propagate interactions for an inhomogeneous system.")
 
-    # Verify bond indices
-    if bond.i == bond.j && iszero(bond.n)
-        error("Bond must connect different sites.")
-    end
-    (1 <= bond.i <= nbasis(sys.crystal)) || error("Atom index $(bond.i) is out of range.")
-    (1 <= bond.j <= nbasis(sys.crystal)) || error("Atom index $(bond.j) is out of range.")
+    ints = interactions(sys)
 
     # Convert J to Mat3
     J = Mat3(J isa Number ? J*I : J)
+    is_heisenberg = isapprox(diagm([J[1,1],J[1,1],J[1,1]]), J; atol=1e-12)
 
     # Verify that exchange is symmetry-consistent
     if !is_coupling_valid(sys.crystal, bond, J)
@@ -133,8 +125,6 @@ function set_exchange!(sys::System{N}, J, bond::Bond) where N
     if any(x -> x.bond == bond, vcat(ints[bond.i].heisen, ints[bond.i].exchange))
         println("Warning: Overriding exchange for bond $bond.")
     end
-
-    is_heisenberg = isapprox(diagm([J[1,1],J[1,1],J[1,1]]), J; atol=1e-12)
 
     for i in 1:nbasis(sys.crystal)
         for (bond′, J′) in zip(all_symmetry_related_couplings_for_atom(sys.crystal, i, bond, J)...)
@@ -206,6 +196,62 @@ function set_biquadratic_at!(sys::System{N}, J, bond::Bond, idx) where N
     @assert idx′[4] == bond′.i
     filter!(c -> c.bond′ != bond′, ints[idx′].biquad)
     push!(ints[idx′].biquad, Coupling(!isculled, bond′, J'))
+
+    return
+end
+
+
+"""
+    set_exchange_at!(sys::System, J, bond::Bond, idx::Site)
+
+Sets an exchange interaction along the [`Bond`](@ref) associated with a single
+[`Site`](@ref). No symmetry propagation will be performed. The system must allow
+inhomogeneous interactions.
+
+Note that `bond` is always defined with respect to the original crystal, whereas
+`idx` is an index into the current [`System`](@ref), which may have been
+reshaped. The atom index `bond.i` must be consistent with the system sublattice
+index `idx[4]`. 
+
+See also [`set_exchange!](@ref), [`to_inhomogeneous`](@ref).
+"""
+function set_exchange_at!(sys::System{N}, J, bond::Bond, idx) where N
+    is_homogeneous(sys) && error("Use `to_inhomogeneous` first.")
+    ints = interactions_inhomog(sys)
+
+    # If system has been reshaped, then we need to transform bond to new
+    # indexing system.
+    bond = transform_bond(sys.crystal, orig_crystal(sys), bond)
+
+    idx = convert_idx(idx)
+    bond.i == idx[4] || error("Atom index `bond.i` is inconsistent with sublattice of `idx`.")
+
+    # Convert J to Mat3
+    J = Mat3(J isa Number ? J*I : J)
+    is_heisenberg = isapprox(diagm([J[1,1],J[1,1],J[1,1]]), J; atol=1e-12)
+    
+    # Add bond in forward direction
+    isculled = bond_parity(bond)
+    filter!(c -> c.bond != bond, ints[idx].heisen)
+    filter!(c -> c.bond != bond, ints[idx].exchange)
+    if is_heisenberg
+        push!(ints[idx].heisen, Coupling(isculled, bond, J[1,1]))
+    else
+        push!(ints[idx].exchange, Coupling(isculled, bond, J))
+    end
+
+    # Add bond in backward direction
+    idx′ = bonded_idx(sys, idx, bond)
+    bond′ = reverse(bond)
+    @assert !isculled == bond_parity(bond′)
+    @assert idx′[4] == bond′.i
+    filter!(c -> c.bond != bond′, ints[idx′].heisen)
+    filter!(c -> c.bond != bond′, ints[idx′].exchange)
+    if is_heisenberg
+        push!(ints[idx′].heisen, Coupling(!isculled, bond′, J'))
+    else
+        push!(ints[idx′].exchange, Coupling(!isculled, bond′, J'))
+    end
 
     return
 end
