2809a5ab8cb1ebb45e8b1024f418251203f09938
Hao Zhang (张浩) <hzhangphys@gmail.com>
2023-03-20 07:14:57 -0700

    Add SU(N) LSWT to Sunny (#68)
    * Initial commit
    
    * Updates for new Sunny interface
    
    * Add supports for :dipole mode for LSWT
    
    * Added spin wave tests
    
    * Removed deepcopy in SWUtil
    
    * Removed WangLandau/
    
    * Updated test_lswt by using polarize_spin!
    
    * Added features for biquad interactions in :dipole
    
    * Renamed function names in the SpinWave module
    
    ---------
    
    Co-authored-by: ddahlbom <david.dahlbom@gmail.com>

---
+++ src/SpinWave/SWUtil.jl
@@ -0,0 +1,175 @@
+###########################################################################
+# Below takes Sunny to construct `SpinWave` for LSWT calculations.  #
+###########################################################################
+
+"""
+Additional fields for linear spin-wave calculations.
+"""
+struct SpinWave
+    sys   :: System
+    s̃_mat :: Array{ComplexF64, 4}  # dipole operators
+    T̃_mat :: Array{ComplexF64, 3}  # single-ion anisos
+    Q̃_mat :: Array{ComplexF64, 4}  # quarupolar operators (for biquad only)
+    chemical_positions :: Vector{Vec3} # positions of magnetic atoms in units of (a₁, a₂, a₃) of the chemical lattice. (useful when computing the dynamical spin structure factor)
+    chemic_reciprocal_basis :: Mat3 # maybe not useful if we have David's interface for S(q, ω)
+    maglat_reciprocal_basis :: Mat3 # reciprocal lattice basis vectors for the magnetic supercell
+    energy_ϵ   :: Float64 # energy epsilon in the diagonalization. Set to add to diagonal elements of the spin-wave Hamiltonian for cholesky decompostion
+    energy_tol :: Float64 # energy tolerance for maximal imaginary part of spin-wave energies
+end
+
+
+"""
+    dipole_to_angles
+
+convert the dipole expectation values from the Cartesian frame to the spherical frame
+"""
+function dipole_to_angles(dipoles :: AbstractVector{Float64})
+    r = norm(dipoles)
+    @assert r > 1e-7
+    θ = acos(dipoles[3] / r)
+    @assert isfinite(θ)
+    ϕ = atan(dipoles[2], dipoles[1])
+    @assert isfinite(ϕ)
+    (ϕ < 0.0) && (ϕ += 2.0 * π)
+    return θ, ϕ
+end
+
+"""
+    generate_local_sun_gens
+
+Compute SU(N) generators in the local reference frame.
+"""
+function generate_local_sun_gens(sys :: System)
+    Nₘ, N = length(sys.dipoles), sys.Ns[1] # number of magnetic atoms and dimension of Hilbert space
+    if sys.mode == :SUN
+        s_mat = spin_matrices(N)
+
+        s̃_mat = Array{ComplexF64, 4}(undef, N, N, 3, Nₘ)
+        T̃_mat = Array{ComplexF64, 3}(undef, N, N, Nₘ)
+        Q̃_mat = zeros(ComplexF64, 0, 0, 0, 0)
+
+        U_mat = Matrix{ComplexF64}(undef, N, N)
+
+        for site = 1:Nₘ
+            U_mat[:, 1] = sys.coherents[1, 1, 1, site]
+            U_mat[:, 2:N] = nullspace(U_mat[:, 1]')
+            @assert isapprox(U_mat * U_mat', I) "rotation matrix from (global frame to local frame) not unitary"
+            for μ = 1:3
+                s̃_mat[:, :, μ, site] = Hermitian(U_mat' * s_mat[μ] * U_mat)
+            end
+            T̃_mat[:, :, site] = Hermitian(U_mat' * sys.interactions_union[site].aniso.matrep * U_mat)
+        end
+
+    elseif sys.mode == :dipole
+        s_mat_2 = spin_matrices(2)
+        s_mat_N = spin_matrices(N)
+        S = (N-1)/2
+
+        # we support the biquad interactions now in the :dipole mode
+        # we choose a particular basis of the nematic operators listed in Appendix B of *Phys. Rev. B 104, 104409*
+        Q_mat = Vector{Matrix{ComplexF64}}(undef, 5)
+        Q_mat[1] = -(s_mat_N[1] * s_mat_N[3] + s_mat_N[3] * s_mat_N[1])
+        Q_mat[2] = -(s_mat_N[2] * s_mat_N[3] + s_mat_N[3] * s_mat_N[2])
+        Q_mat[3] = s_mat_N[1] * s_mat_N[1] - s_mat_N[2] * s_mat_N[2]
+        Q_mat[4] = s_mat_N[1] * s_mat_N[2] + s_mat_N[2] * s_mat_N[1]
+        Q_mat[5] = √3 * s_mat_N[3] * s_mat_N[3] - 1/√3 * S * (S+1) * I
+        
+        s̃_mat = Array{ComplexF64, 4}(undef, 2, 2, 3, Nₘ)
+
+        no_single_ion = isempty(sys.interactions_union[1].aniso.matrep)
+        T̃_mat = no_single_ion ? zeros(ComplexF64, 0, 0, 0) : Array{ComplexF64, 3}(undef, 2, 2, Nₘ)
+        Q̃_mat = Array{ComplexF64, 4}(undef, 2, 2, 5, Nₘ)
+
+        U_mat_2 = Matrix{ComplexF64}(undef, 2, 2)
+        U_mat_N = Matrix{ComplexF64}(undef, N, N)
+
+        for site = 1:Nₘ
+            θ, ϕ = dipole_to_angles(sys.dipoles[1, 1, 1, site])
+            U_mat_N[:] = exp(-1im * ϕ * s_mat_N[3]) * exp(-1im * θ * s_mat_N[2])
+            U_mat_2[:] = exp(-1im * ϕ * s_mat_2[3]) * exp(-1im * θ * s_mat_2[2])
+            @assert isapprox(U_mat_N * U_mat_N', I) "rotation matrix from (global frame to local frame) not unitary"
+            @assert isapprox(U_mat_2 * U_mat_2', I) "rotation matrix from (global frame to local frame) not unitary"
+            for μ = 1:3
+                s̃_mat[:, :, μ, site] = Hermitian(U_mat_2' * s_mat_2[μ] * U_mat_2)
+            end
+            for ν = 1:5
+                Q̃_mat[:, :, ν, site] = Hermitian(U_mat_N' * Q_mat[ν] * U_mat_N)[1:2, 1:2]
+            end
+
+            if !no_single_ion
+                T̃_mat[:, :, site] = Hermitian(U_mat_N' * sys.interactions_union[site].aniso.matrep * U_mat_N)[1:2, 1:2]
+            end
+        end
+    end
+
+    return s̃_mat, T̃_mat, Q̃_mat
+end
+
+"""
+External constructor for `SpinWave`
+"""
+function SpinWave(sys :: System, energy_ϵ :: Float64=1e-8, energy_tol :: Float64=1e-6)
+    s̃_mat, T̃_mat, Q̃_mat = generate_local_sun_gens(sys)
+    maglat_basis = isnothing(sys.origin) ? diagm(ones(3)) : sys.origin.crystal.latvecs \ sys.crystal.latvecs
+
+    Nₘ = length(sys.dipoles)
+    chemical_positions = Vector{Vec3}(undef, Nₘ)
+    for site = 1:Nₘ
+        tmp_pos = maglat_basis * sys.crystal.positions[site]
+        chemical_positions[site] = Vec3(tmp_pos)
+    end
+
+    # computes the reciprocal basis vectors of the magnetic lattice (units 2π/|a|)
+    det_A = det(maglat_basis')
+    maglat_reciprocal_basis = zeros(Float64, 3, 3)
+    maglat_reciprocal_basis[:, 1] = cross(maglat_basis[:, 2], maglat_basis[:, 3]) / det_A
+    maglat_reciprocal_basis[:, 2] = cross(maglat_basis[:, 3], maglat_basis[:, 1]) / det_A
+    maglat_reciprocal_basis[:, 3] = cross(maglat_basis[:, 1], maglat_basis[:, 2]) / det_A
+    maglat_reciprocal_basis = Mat3(maglat_reciprocal_basis)
+
+    # computes the reciprocal basis vectors of the chemical lattice (units Å⁻¹)
+    latvecs = isnothing(sys.origin) ? diagm(ones(3)) : sys.origin.crystal.latvecs
+    det_A = det(latvecs')
+    chemic_reciprocal_basis = zeros(Float64, 3, 3)
+    chemic_reciprocal_basis[:, 1] = cross(latvecs[:, 2], latvecs[:, 3]) / det_A
+    chemic_reciprocal_basis[:, 2] = cross(latvecs[:, 3], latvecs[:, 1]) / det_A
+    chemic_reciprocal_basis[:, 3] = cross(latvecs[:, 1], latvecs[:, 2]) / det_A
+    chemic_reciprocal_basis = Mat3(chemic_reciprocal_basis)
+
+    return SpinWave(sys, s̃_mat, T̃_mat, Q̃_mat, chemical_positions, chemic_reciprocal_basis, maglat_reciprocal_basis, energy_ϵ, energy_tol)
+end
+
+"""
+    k_chemical_to_k_magnetic
+
+Convert the components of a wavevector from the original Brillouin zone (of the chemical lattice) to the reduced Brillouin zone (BZ)
+(of the magnetic lattice). \
+This is necessary because components in the reduced BZ are good quantum numbers.
+`K` is the reciprocal lattice vector, and `k̃` is the components of wavevector in the reduced BZ. Note `k = K + k̃`
+"""
+function k_chemical_to_k_magnetic(sw_fields :: SpinWave, k :: Vector{Float64})
+    α = sw_fields.maglat_reciprocal_basis \ k
+    k̃ = Vector{Float64}(undef, 3)
+    K = Vector{Int}(undef, 3)
+    for i = 1:3
+        if abs(α[i]) < eps()
+            K[i] = 0.0
+            k̃[i] = 0.0
+        else
+            K[i] = Int(round(floor(α[i])))
+            k̃[i] = α[i] - K[i]
+        end
+        @assert k̃[i] ≥ 0.0 && k̃[i] < 1.0
+    end
+    k_check = sw_fields.maglat_reciprocal_basis * (K + k̃)
+    @assert norm(k - k_check) < 1e-12
+
+    return K, k̃
+end
+
+function construct_magnetic_supercell(sys :: System, A :: Matrix{Int})
+    newsys = reshape_geometry(sys, A)
+    mag_latsize = (1, 1, 1)
+    mag_cell_size = cell_dimensions(newsys) * diagm(collect(newsys.latsize))
+    return reshape_geometry_aux(newsys, mag_latsize, mag_cell_size)
+end
---
+++ test/test_lswt.jl
@@ -0,0 +1,166 @@
+@testitem "Single Ion" begin
+    J, J′, D = 1.0, 0.1, 5.0
+
+    a = b = 1.0
+    c = 1.5
+    lat_vecs = lattice_vectors(a, b, c, 90, 90, 90)
+    types = ["A"]
+    basis_vecs = [[0, 0, 0]]
+
+    cryst = Crystal(lat_vecs, basis_vecs; types)
+
+    # Spin System
+    dims = (2, 2, 2)
+    infos = [SpinInfo(1, S=1)]
+    sys = System(cryst, dims, infos, :SUN)
+
+    set_exchange!(sys, J,  Bond(1, 1, [1, 0, 0]))
+    set_exchange!(sys, J′, Bond(1, 1, [0, 0, 1]))
+    set_anisotropy!(sys, D * 𝒮[3]^2, 1)
+
+    Δt  = abs(0.05 / D)
+    λ = 0.1
+    langevin = Langevin(Δt; kT=0, λ)
+
+    randomize_spins!(sys)
+    A = [1 1 1; -1 1 0; 0 0 1]
+    sys_lswt = construct_magnetic_supercell(sys, [1 1 1; -1 1 0; 0 0 1])
+
+    langevin.kT = 0
+    for i in 1:50_000
+        step!(sys_lswt, langevin)
+    end
+
+    sw_fields = SpinWave(sys_lswt)
+
+    function sion_analytical_disp(k :: Vector{Float64})
+        # analytical solutions
+        γkxy = cos(2*π*k[1]) + cos(2*π*k[2])
+        γkz  = cos(2*π*k[3])
+        x = 1/2 - D/(8*(2*J+J′))
+        Ak₊ = -8 * (x-1) * x * (2*J+J′) - (x-1) * D + 2 * (2*x-1) * (J *γkxy + J′*γkz)
+        Bk₊ = -2 * (J * γkxy + J′ * γkz)
+        Ak₋ = -16 * (x-1) * x * (2*J+J′) - (2*x-1) * D - 2 * (1-2*x)^2*(J*γkxy + J′*γkz)
+        Bk₋ = 2 * (1-2*x)^2 * (J*γkxy + J′*γkz)
+        ωk₊ = √(Ak₊^2-Bk₊^2)
+        ωk₋ = √(Ak₋^2-Bk₋^2)
+        return ωk₊, ωk₋
+    end
+
+    k = rand(Float64, 3)
+    ωk1, ωk2 = sion_analytical_disp(k)
+    ωk3, ωk4 = sion_analytical_disp(k .+= 0.5)
+    ωk_ana = [ωk1, ωk2, ωk3, ωk4]
+    index  = sortperm(ωk_ana, rev=true)
+    ωk_ana = ωk_ana[index]
+    ωk_num = dispersion(sw_fields, k)
+
+    @test isapprox(ωk_ana, ωk_num)
+end
+
+@testitem "Intensities" begin
+    using LinearAlgebra
+
+    a = 8.289
+    lat_vecs = lattice_vectors(a, a, a, 90, 90, 90)
+    types = ["MzR1"]
+    basis_vecs = [[0, 0, 0]]
+    fcc = Crystal(lat_vecs, basis_vecs, 225; types)
+    S = 5/2
+
+    # According to a renormalized classical theory for spins (the details will be presented in a manuscript in preparation), the large-S expansion and the :dipole mode should produce the same results when apply the proper renormalization factor for the single-ion interaction strength.
+    cov_factor = (1 - 3/S + 11/(4*S^2)- 3/(4*S^3))
+
+    dims = (1, 1, 1)
+    infos = [SpinInfo(1, S=S)]
+    sys = System(fcc, dims, infos, :dipole)
+
+    J = 22.06 * Sunny.meV_per_K
+    K = 0.15  * Sunny.meV_per_K
+    C = J + K
+    J₁ = diagm([J, J, C])
+    D_ST = 0.2
+    D = D_ST / cov_factor
+
+    set_exchange!(sys, J₁, Bond(1, 2, [0, 0, 0]))
+    Λ = D * (𝒮[1]^4 + 𝒮[2]^4 + 𝒮[3]^4)
+    set_anisotropy!(sys, Λ, 1)
+
+    Δt = abs(0.05 / D)
+    λ  = 0.1
+    langevin = Langevin(Δt; kT=0, λ)
+
+    polarize_spin!(sys, (1, 1, 1), position_to_site(sys, (0, 0, 0)))
+    polarize_spin!(sys, (1, -1, -1), position_to_site(sys, (1/2, 1/2, 0)))
+    polarize_spin!(sys, (-1, -1, 1), position_to_site(sys, (1/2, 0, 1/2)))
+    polarize_spin!(sys, (-1, 1, -1), position_to_site(sys, (0, 1/2, 1/2)))
+    sw_fields = SpinWave(sys)
+
+    disp = zeros(Float64, 4)
+    Sαβ_matrix = zeros(Float64, 4, 9)
+
+    k = [0.8, 0.6, 0.1]
+    Sαβ_matrix =  dssf(sw_fields, k)
+    tmp = Sαβ_matrix[:, 1:3]
+    sunny_trace = sum(tmp, dims=2)
+
+    spintools_trace = [0.0, 1.1743243223274487, 1.229979802236658, 1.048056653379038]
+
+    @test isapprox(sunny_trace, spintools_trace)
+end
+
+@testitem "Biquadratic interactions" begin
+    function test_biquad(k :: Vector{Float64}, S)
+
+        a = 1.0
+        lat_vecs = lattice_vectors(a, a, a, 90, 90, 90)
+        types = ["A"]
+        basis_vecs = [[0, 0, 0]]
+
+        cryst = Crystal(lat_vecs, basis_vecs; types)
+
+        # Spin System
+        dims = (2, 2, 2)
+        infos = [SpinInfo(1, S=S)]
+        sys = System(cryst, dims, infos, :dipole)
+
+        α = -0.4 * π
+        J = 1.0
+        JL, JQ = J * cos(α), J * sin(α) / S^2
+        set_exchange!(sys, JL,  Bond(1, 1, [1, 0, 0]))
+        set_biquadratic!(sys, JQ,  Bond(1, 1, [1, 0, 0]))
+
+        Δt  = abs(0.05 / JL)
+        λ = 0.1
+        langevin = Langevin(Δt; kT=0, λ)
+
+        randomize_spins!(sys)
+
+        langevin.kT = 0
+        for i in 1:100_000
+            step!(sys, langevin)
+        end
+
+        sys_lswt = construct_magnetic_supercell(sys, [1 1 1; -1 1 0; 0 0 1])
+        langevin.kT = 0
+
+        for i in 1:10_000
+            step!(sys_lswt, langevin)
+        end
+
+        sw_fields = SpinWave(sys_lswt)
+
+        @inline γk(k :: Vector{Float64}) = 2 * (cos(2π*k[1]) + cos(2π*k[2]) + cos(2π*k[3]))
+        @inline ϵk₁(k :: Vector{Float64}) = J * (S*cos(α) - (2*S-2+1/S) * sin(α)) * √(36 - γk(k)^2) 
+
+        ϵk_num = dispersion(sw_fields, k)
+        ϵk_ana = ϵk₁(k)
+
+        isapprox(ϵk_num[1], ϵk_ana)
+    end
+
+    k = rand(Float64, 3)
+    @test test_biquad(k, 1)
+    @test test_biquad(k, 3/2)
+
+end
