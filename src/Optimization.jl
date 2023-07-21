ce78d7d3fd5419b3ea35d7fad0a1593ac36877a0
ddahlbom <david.dahlbom@gmail.com>
2023-07-21 16:13:12 -0400

    Jacobian function (with charts)

--- src/Optimization.jl
+++ src/Optimization.jl
@@ -49,18 +49,26 @@ end
 # Coordinate systems
 ################################################################################
 
+using LinearAlgebra, StaticArrays, BenchmarkTools
 struct StereographicPoint{Nm1}
     chart :: Int
-    coord :: SVector{Nm1, Float64}
+    coord :: NTuple{Nm1, Float64}
 end
 
-function vec_to_stereo(vec::SVector{N, Float64}) where N  # N parameter unnecessary here, but generalizes to ℝP^{N-1}
-    remaining_indices(skip, max) = ntuple(i -> i > (skip-1) ? i + 1 : i, max-1)
+function Base.show(io::IO, ::MIME"text/plain", sp::StereographicPoint{Nm1}) where Nm1
+    println(io, "($(sp.coord[1]), $(sp.coord[2])) in ⟂$(sp.chart)-plane")
+end
 
-    plane_idx = argmax(vec)               
-    is = remaining_indices(plane_idx, N)   
+@inline δ(i,j) = i == j ? 1 : 0
+@inline remaining_indices(skip, max) = ntuple(i -> i > (skip-1) ? i + 1 : i, max-1)
+toNidx(i, c) = i > c ? i - 1 : i
+
+function vec_to_stereo(vec::SVector{N, Float64}) where N  # N parameter unnecessary here, but generalizes to ℝP^{N-1}
+    plane_idx = argmin(vec) # Project onto plane perpendicular to axis corresponding
+                            # to the minimal vector component, i.e. ⟂ ̂x_min
+    other_idxs = remaining_indices(plane_idx, N)   
     denom = 1 - vec[plane_idx]             
-    StereographicPoint{N-1}(plane_idx, ntuple(i -> vec[is[i]]/denom, N-1)) 
+    StereographicPoint{N-1}(plane_idx, ntuple(i -> vec[other_idxs[i]]/denom, N-1)) 
 end
 
 function stereo_to_vec(sp::StereographicPoint{Nm1}) where Nm1
@@ -70,7 +78,7 @@ function stereo_to_vec(sp::StereographicPoint{Nm1}) where Nm1
             if i < sp.chart
                 2sp.coord[i]/(1+r2)
             elseif i == sp.chart
-                (-1 + r2)/(1+r2)
+                (-1+r2)/(1+r2)
             else
                 2sp.coord[i-1]/(1+r2)
             end
@@ -78,21 +86,61 @@ function stereo_to_vec(sp::StereographicPoint{Nm1}) where Nm1
     )
 end
 
+@inline remap_stereo(sp) = sp |> stereo_to_vec |> vec_to_stereo
+
+function stereo_jac!(jac, sp::StereographicPoint{Nm1}) where Nm1 # Hardy-har
+    c, X = sp.chart, sp.coord
+    r2 = mapreduce(x -> x^2, +, X)
+    for j in 1:Nm1+1, k in 1:Nm1
+        jac[k,j] = if j == c
+            (2X[k]/(r2+1))*(1 + (r2-1)/(r2+1))
+        else
+            (2/(r2+1))*(δ(j,k) + (2X[toNidx(j,c)]*X[k])/(r2+1))
+        end
+    end
+    jac
+end
+
+function stereo_jac(sp::StereographicPoint{Nm1}) where Nm1
+    jac = zeros(Nm1, Nm1+1)
+    @time stereo_jac!(jac, sp)
+end
 
 
-# Tests
-# for _ in 1:20
-#     N = 3
-#     vec = SVector{N, Float64}(normalize(rand(N)))
-#     sp = vec_to_stereo(vec)
-#     println(stereo_to_vec(sp) ≈ vec ? "Good" : "Bad")
-# end
-# 
-# begin
-#     N = 3
-#     vec = SVector{N, Float64}(normalize(rand(N)))
-#     sp = vec_to_stereo(vec)
-#     @btime vec_to_stereo($vec)
-#     vec1 = stereo_to_vec(sp)
-#     @btime stereo_to_vec($sp)
-# end
+
+## Tests
+# Test display
+for _ in 1:10
+    N = 3
+    vec = SVector{N, Float64}(normalize(rand(N)))
+    sp = vec_to_stereo(vec)
+    display(sp)
+end
+
+# Test invertibility
+for _ in 1:20
+    N = 3
+    vec = SVector{N, Float64}(normalize(rand(N)))
+    sp = vec_to_stereo(vec)
+    println(stereo_to_vec(sp) ≈ vec ? "Good" : "Bad")
+end
+
+# Test reparameterization
+begin
+    sp = StereographicPoint{2}(1, (2.1, 0.2))
+    vec = stereo_to_vec(sp)
+    sp2 = remap_stereo(sp)
+end
+
+# Benchmark
+begin
+    N = 3
+    vec = SVector{N, Float64}(normalize(rand(N)))
+    sp = vec_to_stereo(vec)
+    @btime vec_to_stereo($vec)
+    vec1 = stereo_to_vec(sp)
+    @btime stereo_to_vec($sp)
+    sp2 = remap_stereo(sp)
+    @btime remap_stereo($sp)
+end
+
