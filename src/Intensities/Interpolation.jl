f56cf2bbef20d258b6b418dd5b541095f90673d9
Sam Quinn <lazersmoke@gmail.com>
2023-08-14 15:13:12 -0400

    Simplify interpolation internals

--- src/Intensities/Interpolation.jl
+++ src/Intensities/Interpolation.jl
@@ -3,21 +3,15 @@ struct NoInterp <: InterpolationScheme{1} end
 struct LinearInterp <: InterpolationScheme{8} end
 
 
-function interpolated_intensity(::SampledCorrelations, _, _, stencil_intensities, ::NoInterp) 
+function interpolated_intensity(::SampledCorrelations, _, stencil_intensities, ::NoInterp) 
     return only(stencil_intensities)
 end
 
-function interpolated_intensity(::SampledCorrelations, q_target, qs, stencil_intensities, ::LinearInterp) 
-    q000,    _,    _,    _,    _,    _,    _, q111 = qs 
+# N.B.: This interpolation is only valid when all three entries of m_target are in [0,1]
+function interpolated_intensity(::SampledCorrelations, m_target, stencil_intensities, ::LinearInterp) 
     c000, c100, c010, c110, c001, c101, c011, c111 = stencil_intensities
 
-    x, y, z = q_target
-    x0, y0, z0 = q000 
-    x1, y1, z1 = q111 
-
-    xd = (x-x0)/(x1-x0)
-    yd = (y-y0)/(y1-y0)
-    zd = (z-z0)/(z1-z0)
+    xd, yd, zd = m_target
 
     c00 = c000*(1-xd) + c100*xd
     c01 = c001*(1-xd) + c101*xd
@@ -29,7 +23,6 @@ function interpolated_intensity(::SampledCorrelations, q_target, qs, stencil_int
 
     return c0*(1-zd) + c1*zd
 end
-
 
 function stencil_points(sc::SampledCorrelations, q, ::NoInterp)
     Ls = size(sc.samplebuf)[2:4] 
@@ -95,16 +88,12 @@ function pruned_stencil_info(sc::SampledCorrelations, qs, interp::InterpolationS
     end
     @assert sum(counts) == length(m_info)
 
-    # Calculate corresponding q (RLU) and k (global) vectors
-    qs_all = map(ms_all) do ms
-       map(m -> m ./ sc.latsize, ms) 
+    # Calculate corresponding wave vectors in absolute units
+    ks_all = map(ms_all) do ms
+        map(m -> sc.crystal.recipvecs * (m ./ sc.latsize), ms)
     end
 
-    ks_all = map(qs_all) do qs
-        map(q -> sc.crystal.recipvecs * q, qs) # FIXME: should q still by in sc.crystal, or in absolute units?
-    end
-    
-    return (; qs_all, ks_all, idcs_all, counts)
+    return (; ks_all, idcs_all, counts)
 end
 
 
@@ -168,21 +157,26 @@ function intensities_interpolated(sc::SampledCorrelations, qs;
 end
 
 
-# Actual intensity calculation
 function intensities_interpolated!(intensities, sc::SampledCorrelations, q_targets::Array, ωvals, interp::InterpolationScheme{NInterp}, formula, stencil_info, T) where {NInterp}
     li_intensities = LinearIndices(intensities)
-    ci_qs = CartesianIndices(q_targets)
-    (; qs_all, ks_all, idcs_all, counts) = stencil_info 
+    ci_targets = CartesianIndices(q_targets)
+
+    # Compute the location of each q_target within its cell in R.L.U.
+    # (which is an axis-aligned unit cube) with coordinates given as
+    # the fractional distance along each side of the cube.
+    m_targets = [mod.(sc.latsize .* q_target,1) for q_target in q_targets]
+
+    (; ks_all, idcs_all, counts) = stencil_info 
     for iω in eachindex(ωvals)
         iq = 0
-        for (qs, ks, idcs, numrepeats) in zip(qs_all, ks_all, idcs_all, counts)
+        for (ks, idcs, numrepeats) in zip(ks_all, idcs_all, counts)
             # The closure `formula.calc_intensity` is defined in
             # intensity_formula(f, ::SampledCorrelations, ...)
             local_intensities = SVector{NInterp, T}(formula.calc_intensity(sc, ks[n], idcs[n], iω) for n in 1:NInterp)
             for _ in 1:numrepeats
                 iq += 1
-                idx = li_intensities[CartesianIndex(ci_qs[iq], iω)]
-                intensities[idx] = interpolated_intensity(sc, q_targets[iq], qs, local_intensities, interp) 
+                idx = li_intensities[CartesianIndex(ci_targets[iq], iω)]
+                intensities[idx] = interpolated_intensity(sc, m_targets[iq], local_intensities, interp) 
             end
         end
     end
