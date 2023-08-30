f35d627381693b38d9f4b25949ac8c54fff3706b
Kipton Barros <kbarros@gmail.com>
2023-08-29 21:46:22 -0600

    Progress

--- src/SampledCorrelations/FormFactor.jl
+++ src/SampledCorrelations/FormFactor.jl
@@ -1,4 +1,7 @@
-# Expansion of the form `A exp(-a s^2) + B exp(-b s^2) + C exp(-c s^2) + D exp(-d s^2) + E`
+
+include("FormFactorData.jl")
+
+# `A exp(-a s^2) + B exp(-b s^2) + C exp(-c s^2) + D exp(-d s^2) + E`
 struct ExpandedBesselIntegral
     A :: Float64
     a :: Float64
@@ -14,6 +17,7 @@ end
 struct FormFactor
     j0 :: ExpandedBesselIntegral
     j2 :: ExpandedBesselIntegral
+    config :: String
     g :: Float64
 end
 
@@ -21,7 +25,7 @@ const identity_form_factor = let
     j0 = ExpandedBesselIntegral(0, 0, 0, 0, 0, 0, 0, 0, 1)
     j2 = ExpandedBesselIntegral(0, 0, 0, 0, 0, 0, 0, 0, 0)
     g = 2
-    FormFactor(j0, j2, g)
+    FormFactor(j0, j2, "", g)
 end
 
 """
@@ -32,71 +36,72 @@ to an [`intensity_formula`](@ref), determines a ``|𝐪|``-dependent scaling of
 the structure factor.
 
 The parameter `ion` must be one of the following strings:
+
 ```
-Sc0,Sc1,Sc2,Ti0,Ti1,Ti2,Ti3,V0,V1,V2,V3,V4,Cr0,Cr1,Cr2,Cr3,Cr4,Mn0,Mn1,Mn2,Mn3,
-Mn4,Fe0,Fe1,Fe2,Fe3,Fe4,Co0,Co1,Co2,Co3,Co4,Ni0,Ni1,Ni2,Ni3,Ni4,Cu0,Cu1,Cu2,Cu3,
-Cu4,Y0,Zr0,Zr1,Nb0,Nb1,Mo0,Mo1,Tc0,Tc1,Ru0,Ru1,Rh0,Rh1,Pd0,Pd1,Ce2,Nd2,Nd3,Sm2,
-Sm3,Eu2,Eu3,Gd2,Gd3,Tb2,Tb3,Dy2,Dy3,Ho2,Ho3,Er2,Er3,Tm2,Tm3,Yb2,Yb3,Pr3,U3,U4,
-U5,Np3,Np4,Np5,Np6,Pu3,Pu4,Pu5,Pu6,Am2,Am3,Am4,Am5,Am6,Am7
+Am2, Am3, Am4, Am5, Am6, Am7, Au1, Au2, Au3, Au4, Au5, Ce2, Co0, Co1, Co2, Co3,
+Co4, Cr0, Cr1, Cr2, Cr3, Cr4, Cu0, Cu1, Cu2, Cu3, Cu4, Dy2, Dy3, Er2, Er3, Eu2,
+Eu3, Fe0, Fe1, Fe2, Fe3, Fe4, Gd2, Gd3, Hf2, Hf3, Ho2, Ho3, Ir0a, Ir0b, Ir0c,
+Ir1a, Ir1b, Ir2, Ir3, Ir4, Ir5, Ir6, Mn0, Mn1, Mn2, Mn3, Mn4, Mo0, Mo1, Nb0,
+Nb1, Nd2, Nd3, Ni0, Ni1, Ni2, Ni3, Ni4, Np3, Np4, Np5, Np6, Os0a, Os0b, Os0c,
+Os1a, Os1b, Os2, Os3, Os4, Os5, Os6, Os7, Pd0, Pd1, Pr3, Pt1, Pt2, Pt3, Pt4,
+Pt5, Pt6, Pu3, Pu4, Pu5, Pu6, Re0a, Re0b, Re0c, Re1a, Re1b, Re2, Re3, Re4, Re5,
+Re6, Rh0, Rh1, Ru0, Ru1, Sc0, Sc1, Sc2, Sm2, Sm3, Ta2, Ta3, Ta4, Tb2, Tb3, Tc0,
+Tc1, Ti0, Ti1, Ti2, Ti3, Tm2, Tm3, U3, U4, U5, V0, V1, V2, V3, V4, W0a, W0b,
+W0c, W1a, W1b, W2c, W3, W4, W5, Y0, Yb2, Yb3, Zr0, Zr1
 ```
-(data taken from Refs. [1] and [2]) or
-```
-Ir3
-```
-(data taken from Ref. [3]).
 
 The trailing number denotes ionization state. For example, "Fe0" denotes a
-neutral atom, while "Fe2" denotes ``Fe\\_2\\^+``.
+neutral iron atom, while "Fe2" denotes ``Fe_2^+``. If multiple electronic
+configurations are possible, they will be distinguished by a trailing index (a,
+b, ...). Details are given in the `config` field, e.g.,
 
-The electron state can be characterized by a radial integral ``⟨j_l(s)⟩``
-associated with the ``l``th Bessel function of the magnetic dipole, where ``s =
-|k|/4π``, and ``|k|`` is the magnitude of momentum transfer. The approximation
-to the form factor used by Sunny is 
+```julia
+println(FormFactor("Ir0a").config) # 6s⁰5d⁹
+println(FormFactor("Ir0b").config) # 6s¹5d⁸
+```
+
+The form factor is approximated as
 
 ``F(s) = ⟨j_0(s)⟩ + \\frac{2-g}{g} ⟨j_2(s)⟩ s^2``,
 
-which involves the Landé ``g``-factor if distinct from 2. The radial integrals
-``⟨j_l(s)⟩`` have been estimated using Hartree-Fock for transition metals, or
-Dirac-Fock for the rare earths and actinide series.
+involving the Landé ``g``-factor. The ``⟨j_l(s)⟩`` are radial integrals
+associated with the ``l``th Bessel function of the magnetic dipole, where ``s =
+|k|/4π``, and ``|k|`` is the magnitude of momentum transfer. 
 
-Sunny employs Gaussian fits to ``j_0`` and ``j_2`` of the functional form
+The radial integrals have been calculated using Hartree-Fock for transition
+metals, or Dirac-Fock for the rare earths and actinide series [1--3]. Sunny uses
+approximate fits as a sum of Gaussians,
 
-``⟨j_l(s)⟩ = A e^{-as^2} + B e^{-bs^2} + C e^{-cs^2} + D e^{-ds^2} + E.``
+```math
+⟨j_0(s)⟩ = A e^{-as^2} + B e^{-bs^2} + C e^{-cs^2} + D e^{-ds^2} + E
+⟨j_l(s)⟩ = (A e^{-as^2} + B e^{-bs^2} + C e^{-cs^2} + D e^{-ds^2} + E) s^2
+```
 
 References:
 
- 1. [](https://www.ill.eu/sites/ccsl/ffacts/ffachtml.html)
+ 1. https://www.ill.eu/sites/ccsl/ffacts/ffachtml.html
  2. J. Brown, The Neutron Data Booklet, 2nd ed., Sec. 2.5 Magnetic Form Factors
     (2003)
  3. K. Kobayashi, T. Nagao, M. Ito, Acta Cryst. A, 67 pp 473–480 (2011)
 """
 function FormFactor(ion::String; g_lande=2)
-    function lookup_ff_params(ion, datafile)
-        path = joinpath(@__DIR__, "data", datafile)
-        lines = collect(eachline(path))
-        matches = filter(line -> startswith(line, ion), lines)
-        if isempty(matches)
-            error("'$ion' not a valid magnetic ion.")
-        end
-        ExpandedBesselIntegral(parse.(Float64, split(matches[1])[2:end])...)
-    end
-
-    j0 = lookup_ff_params(ion, "form_factor_J0.dat")
-    j2 = lookup_ff_params(ion, "form_factor_J2.dat")
-    FormFactor(j0, j2, g_lande)
+    (j0, j2, config) = radial_integral_coefficients[ion]
+    j0 = ExpandedBesselIntegral(j0...)
+    j2 = ExpandedBesselIntegral(j2...)
+    FormFactor(j0, j2, config, g_lande)
 end
 
 
 function compute_gaussian_expansion(j::ExpandedBesselIntegral, s2)
-    (; A, a, B, b, C, c, D) = j
-    return A*exp(-a*s2) + B*exp(-b*s2) + C*exp(-c*s2) + D
+    (; A, a, B, b, C, c, D, d, E) = j
+    return A*exp(-a*s2) + B*exp(-b*s2) + C*exp(-c*s2) + D*exp(-d*s2) + E
 end
 
 function compute_form_factor(form_factor::FormFactor, k2_absolute::Float64)
     (; j0, j2, g) = form_factor
 
     # Return early if this is the identity form factor
-    (j0.A == j0.B == j0.C == 0) && (j0.D == 1) && (g == 2) && return 1.0
+    (j0.A == j0.B == j0.C == j0.D == 0) && (j0.E == 1) && (g == 2) && return 1.0
 
     s2 = k2_absolute / (4π)^2
     if g == 2
