7bfb9b7d6a9cdf1be55963a1c1f8bdc3a1796db9
ddahlbom <david.dahlbom@gmail.com>
2023-11-01 15:38:49 -0400

    Add `set_spin_rescaling!`

--- src/Sunny.jl
+++ src/Sunny.jl
@@ -77,10 +77,10 @@ include("System/OnsiteCoupling.jl")
 include("System/Ewald.jl")
 include("System/Interactions.jl")
 export SpinInfo, System, Site, eachsite, position_to_site, global_position, magnetic_moment, 
-    set_coherent!, set_dipole!, polarize_spins!, randomize_spins!, energy, energy_per_site, spin_label,
-    set_onsite_coupling!, set_pair_coupling!, set_exchange!, dmvec, enable_dipole_dipole!, set_external_field!,
-    to_inhomogeneous, set_external_field_at!, set_vacancy_at!, set_onsite_coupling_at!, set_exchange_at!,
-    set_pair_coupling_at!, symmetry_equivalent_bonds, remove_periodicity!
+    set_coherent!, set_dipole!, polarize_spins!, randomize_spins!, set_spin_rescaling!, energy, energy_per_site,
+    spin_label, set_onsite_coupling!, set_pair_coupling!, set_exchange!, dmvec, enable_dipole_dipole!,
+    set_external_field!, to_inhomogeneous, set_external_field_at!, set_vacancy_at!, set_onsite_coupling_at!,
+    set_exchange_at!, set_pair_coupling_at!, symmetry_equivalent_bonds, remove_periodicity!
 
 include("MagneticOrdering.jl")
 export print_wrapped_intensities, suggest_magnetic_supercell, set_spiral_order!, set_spiral_order_on_sublattice!
