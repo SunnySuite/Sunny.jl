182a6cf2e36f6e51efee72969b2944cdaad6aeae
Kipton Barros <kbarros@gmail.com>
2023-11-25 20:04:34 -0700

    Disable persistent tasks for speed

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,7 +1,7 @@
 
 @testitem "Aqua" begin
     import Aqua
-    Aqua.test_all(Sunny; ambiguities=false)
+    Aqua.test_all(Sunny; ambiguities=false, persistent_tasks=false)
 
     # Test ambiguities just for Sunny 
     @test isempty(detect_ambiguities(Sunny; recursive=false))
