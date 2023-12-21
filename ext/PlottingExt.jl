93d86688a7a0b6e560649e4e16198d1c670a54ba
Kipton Barros <kbarros@gmail.com>
2023-12-21 15:20:57 -0700

    JMol atom colors

--- ext/PlottingExt.jl
+++ ext/PlottingExt.jl
@@ -1,10 +1,11 @@
 module PlottingExt
 
 using Sunny
-import Sunny: Vec3, orig_crystal, natoms # Private functions
-
+import Sunny: Vec3, orig_crystal, natoms
 using LinearAlgebra
 import Makie
+
+getindex_cyclic(a, i) = a[mod1(i, length(a))] 
 
 const seaborn_bright = [
     Makie.RGBf(0.00784313725490196,0.24313725490196078,1.0),
@@ -32,7 +33,67 @@ const seaborn_muted = [
     Makie.RGBf(0.5098039215686274,0.7764705882352941,0.8862745098039215),
 ]
 
-getindex_cyclic(a, i) = a[mod1(i, length(a))] 
+# Colors from Jmol table, https://jmol.sourceforge.net/jscolors/
+atom_colors = let
+    pairs = [
+        "H" =>"#FFFFFF", "He"=>"#D9FFFF", "Li"=>"#CC80FF", "Be"=>"#C2FF00",
+        "B" =>"#FFB5B5", "C"=> "#909090", "N" =>"#3050F8", "O" =>"#FF0D0D",
+        "F" =>"#90E050", "Ne"=>"#B3E3F5", "Na"=>"#AB5CF2", "Mg"=>"#8AFF00",
+        "Al"=>"#BFA6A6", "Si"=>"#F0C8A0", "P" =>"#FF8000", "S"=>"#FFFF30",
+        "Cl"=>"#1FF01F", "Ar"=>"#80D1E3", "K" =>"#8F40D4", "Ca"=>"#3DFF00",
+        "Sc"=>"#E6E6E6", "Ti"=>"#BFC2C7", "V" =>"#A6A6AB", "Cr"=>"#8A99C7",
+        "Mn"=>"#9C7AC7", "Fe"=>"#E06633", "Co"=>"#F090A0", "Ni"=>"#50D050",
+        "Cu"=>"#C88033", "Zn"=>"#7D80B0", "Ga"=>"#C28F8F", "Ge"=>"#668F8F",
+        "As"=>"#BD80E3", "Se"=>"#FFA100", "Br"=>"#A62929", "Kr"=>"#5CB8D1",
+        "Rb"=>"#702EB0", "Sr"=>"#00FF00", "Y"=>"#94FFFF",  "Zr"=>"#94E0E0",
+        "Nb"=>"#73C2C9", "Mo"=>"#54B5B5", "Tc"=>"#3B9E9E", "Ru"=>"#248F8F",
+        "Rh"=>"#0A7D8C", "Pd"=>"#006985", "Ag"=>"#C0C0C0", "Cd"=>"#FFD98F",
+        "In"=>"#A67573", "Sn"=>"#668080", "Sb"=>"#9E63B5", "Te"=>"#D47A00",
+        "I" =>"#940094", "Xe"=>"#429EB0", "Cs"=>"#57178F", "Ba"=>"#00C900",
+        "La"=>"#70D4FF", "Ce"=>"#FFFFC7", "Pr"=>"#D9FFC7", "Nd"=>"#C7FFC7",
+        "Pm"=>"#A3FFC7", "Sm"=>"#8FFFC7", "Eu"=>"#61FFC7", "Gd"=>"#45FFC7",
+        "Tb"=>"#30FFC7", "Dy"=>"#1FFFC7", "Ho"=>"#00FF9C", "Er"=>"#00E675",
+        "Tm"=>"#00D452", "Yb"=>"#00BF38", "Lu"=>"#00AB24", "Hf"=>"#4DC2FF",
+        "Ta"=>"#4DA6FF", "W" =>"#2194D6", "Re"=>"#267DAB", "Os"=>"#266696",
+        "Ir"=>"#175487", "Pt"=>"#D0D0E0", "Au"=>"#FFD123", "Hg"=>"#B8B8D0",
+        "Tl"=>"#A6544D", "Pb"=>"#575961", "Bi"=>"#9E4FB5", "Po"=>"#AB5C00",
+        "At"=>"#754F45", "Rn"=>"#428296", "Fr"=>"#420066", "Ra"=>"#007D00",
+        "Ac"=>"#70ABFA", "Th"=>"#00BAFF", "Pa"=>"#00A1FF", "U"=>"#008FFF",
+        "Np"=>"#0080FF", "Pu"=>"#006BFF", "Am"=>"#545CF2", "Cm"=>"#785CE3",
+        "Bk"=>"#8A4FE3", "Cf"=>"#A136D4", "Es"=>"#B31FD4", "Fm"=>"#B31FBA",
+        "Md"=>"#B30DA6", "No"=>"#BD0D87", "Lr"=>"#C70066", "Rf"=>"#CC0059",
+        "Db"=>"#D1004F", "Sg"=>"#D90045", "Bh"=>"#E00038", "Hs"=>"#E6002E",
+        "Mt"=>"#EB0026",
+    ]
+    # Workaround for absence of `mapvalues` in Julia
+    Dict(map(pairs) do (n, c)
+        (lowercase(n), Makie.color(c))
+    end)
+end
+
+function type_to_color(t::String)
+    letters = vcat('a':'z', 'A':'Z')
+    idx = findfirst(c -> !in(c, letters), t)
+    elem = isnothing(idx) ? t : t[begin:idx-1]
+    return get(atom_colors, lowercase(elem), nothing)
+end
+
+function build_class_colors(cryst::Crystal)
+    cnt = 0
+    ret = Dict{Int, Makie.RGB}()
+    root = @something cryst.root cryst
+    for (type, class) in zip(root.types, root.classes)
+        if !haskey(ret, class)
+            color = type_to_color(type)
+            if isnothing(color)
+                cnt += 1
+                color = getindex_cyclic(seaborn_muted, cnt)
+            end
+            push!(ret, class => color)
+        end
+    end
+    return ret
+end
 
 # Analogous to internal Makie function `numbers_to_colors`
 function numbers_to_colors!(out::AbstractArray{Makie.RGBAf}, in::AbstractArray{<: Number}, colormap, colorrange)
@@ -413,9 +474,8 @@ function Sunny.view_crystal(cryst::Crystal, max_dist; orthographic=false,
     # set a scale for the scene in case there is only one atom).
     Makie.linesegments!(ax, cell_wireframe(cryst.latvecs, dims); color=:teal, linewidth=1.5, inspectable=false)
 
-    # Map atom classes to indices that run from 1..nclasses
-    unique_classes = unique(cryst.classes)
-    class_indices = [findfirst(==(c), unique_classes) for c in cryst.classes]
+    # Dict that maps atom class to color
+    class_colors = build_class_colors(cryst)
 
     # Show atoms
     ℓ0 = characteristic_length_between_atoms(cryst)
@@ -430,7 +490,7 @@ function Sunny.view_crystal(cryst::Crystal, max_dist; orthographic=false,
             # If drawing ghosts, require !iszero(n), and vice versa
             iszero(n) == isghost && continue
             push!(pts, cryst.latvecs * (cryst.positions[i] + n))
-            push!(color, getindex_cyclic(seaborn_muted, class_indices[i]))
+            push!(color, class_colors[cryst.classes[i]])
         end
         Makie.meshscatter!(ax, pts; markersize, color, alpha, diffuse=1.15, inspectable=false, transparency=isghost)
 
