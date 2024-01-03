3404f8e98f7981ac078f0e09119c1faf13df47d1
Kipton Barros <kbarros@gmail.com>
2024-01-03 11:25:38 -0700

    Disable WGLMakie by default

--- docs/make.jl
+++ docs/make.jl
@@ -51,8 +51,12 @@ function build_examples(example_sources, destdir)
     # will be stored in the `assets/` directory of the hosted docs.
     for source in example_sources
         function preprocess(str)
-            # Notebooks use WGLMakie instead of GLMakie
+            # Ideally, notebooks would use WGLMakie instead of GLMakie. There
+            # are currently too many bugs to enable by default:
+            # https://github.com/SunnySuite/Sunny.jl/issues/211
+            #=
             str = replace(str, r"^using(.*?)GLMakie"m => s"using\1WGLMakie")
+            =#
         end
         # Build notebooks
         Literate.notebook(source, notebooks_path; preprocess, execute=false, credit=false)
