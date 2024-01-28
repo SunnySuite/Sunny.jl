e1b0c6542d75d39115c0002dec8b8fdb2984aa47
Kipton Barros <kbarros@gmail.com>
2024-01-28 10:44:16 -0700

    Aqua acceleration

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,8 +1,18 @@
 
 @testitem "Aqua" begin
     import Aqua
-    Aqua.test_all(Sunny; ambiguities=false, persistent_tasks=false)
+    Aqua.test_unbound_args(Sunny)
+    Aqua.test_undefined_exports(Sunny)
+    Aqua.test_project_extras(Sunny)
+    Aqua.test_deps_compat(Sunny; check_julia=true, check_extras=true, check_weakdeps=true)
+    Aqua.test_piracies(Sunny)
 
-    # Test ambiguities just for Sunny 
-    @test isempty(detect_ambiguities(Sunny; recursive=false))
+    #=
+        # These take ~30s because they require a fresh Julia process
+        Aqua.test_ambiguities(Sunny; recursive=false)
+        Aqua.test_stale_deps(Sunny)
+
+        # This takes 6s and is unlikely to be triggered by Sunny
+        Aqua.test_persistent_tasks(Sunny)
+    =#
 end
