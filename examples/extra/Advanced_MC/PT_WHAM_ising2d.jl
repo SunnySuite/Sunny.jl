a0766ff146105d81fc11efb0ccac1fc97a6d3744
Kipton Barros <kbarros@gmail.com>
2024-02-20 23:19:32 -0500

    Style tweaks to advanced Monte Carlo

--- examples/extra/Advanced_MC/PT_WHAM_ising2d.jl
+++ examples/extra/Advanced_MC/PT_WHAM_ising2d.jl
@@ -1,64 +1,62 @@
-using Sunny, Plots
+using Sunny, GLMakie
 
-# create 2D Ising model
-a = 1
-latvecs = lattice_vectors(a,a,10a,90,90,90)
+# Square lattice
+latvecs = lattice_vectors(1, 1, 10, 90, 90, 90)
 crystal = Crystal(latvecs, [[0,0,0]])
 
+# 20×20 sites, ferromagnetic exchange
 L = 20
 sys = System(crystal, (L,L,1), [SpinInfo(1, S=1, g=1)], :dipole, units=Units.theory, seed=0)
 polarize_spins!(sys, (0,0,1))
-
 set_exchange!(sys, -1.0, Bond(1,1,(1,0,0)))
 
-# temperature schedule for thermodynamics
+# Temperature schedule for parallel tempering
 kT_min = 0.5
 kT_max = 10.0
 n_replicas = 40 # can be larger than number of cores on machine
 kT_sched = collect(range(kT_min, kT_max, length=n_replicas))
 
-# use Metropolis sampler for Ising system
+# Metropolis sampler with local spin flips
 sampler = LocalSampler(; kT=0, propose=propose_flip)
 
-# initialize parallel tempering 
+# Parallel tempering sampler
 PT = Sunny.ParallelTempering(sys, sampler, kT_sched)
-
-# sampling parameters
 n_therm = 1000
 n_measure = 200
 measure_interval = 10
 exch_interval = 5
 
-# energy histograms for each PT replica
+# Energy histograms for each PT replica
 E_hists = [Sunny.Histogram() for _ in 1:PT.n_replicas]
 
-# equilibration
+# Initial equilibration
 Sunny.step_ensemble!(PT, n_therm, exch_interval)
 
-# start PT simulation
+# Start PT simulation
 for _ in 1:n_measure
-    # run some sweeps and replica exchanges
+    # Run some sweeps and replica exchanges
     Sunny.step_ensemble!(PT, measure_interval, exch_interval)
 
-    # measurements - assuming LocalSampler used
+    # Measurements - assuming LocalSampler used
     for (j, sampler) in enumerate(PT.samplers)
         E_hists[j][sampler.ΔE] += 1
     end
 end
 
-# acceptance rates for (rank, rank+1)
+# Acceptance rates for (rank, rank+1)
 A = PT.n_accept ./ PT.n_exch
 
-# apply multiple histogram reweighting to PT data to get ln_g(E)
+# Apply multiple histogram reweighting to PT data to get ln_g(E)
 E, ln_g = Sunny.WHAM(E_hists, PT.kT_sched; n_iters=1000)
 
-# calculate thermodynamics
+# Derived thermodynamic properties
 kT = collect(range(kT_min, kT_max, length=1000))
 U = Sunny.ensemble_average(E, ln_g, E, kT)
 U² = Sunny.ensemble_average(E, ln_g, E.^2, kT)
 C = @. abs( (U² - U^2) / kT^2 )
 
-# plot density of states and thermodynamics
-display( plot(E/L^2, ln_g .- minimum(ln_g), xlabel="E/N", ylabel="ln[g(E)]", legend=false) )
-display( plot(kT, U/L^2, xlabel="kT/J", ylabel="U/N", legend=false) )
-display( plot(kT, C/L^2, xlabel="kT/J", ylabel="C/N", legend=false) )
+# Plots
+fig = Figure()
+lines(fig[1,1], E/L^2, ln_g .- minimum(ln_g); axis=(xlabel="E/N", ylabel="ln[g(E)]"))
+lines(fig[2,1], kT, U/L^2; axis=(xlabel="kT/J", ylabel="U/N"))
+lines(fig[3,1], kT, C/L^2; axis=(xlabel="kT/J", ylabel="C/N"))
--- examples/extra/Advanced_MC/REWL_ising2d.jl
+++ examples/extra/Advanced_MC/REWL_ising2d.jl
@@ -1,36 +1,32 @@
-using Sunny, Plots
+using Sunny, GLMakie
 
-# create 2D Ising model
-a = 1
-latvecs = lattice_vectors(a,a,10a,90,90,90)
+# Square lattice
+latvecs = lattice_vectors(1, 1, 10, 90, 90, 90)
 crystal = Crystal(latvecs, [[0,0,0]])
 
+# 20×20 sites, ferromagnetic exchange
 L = 20
 sys = System(crystal, (L,L,1), [SpinInfo(1, S=1, g=1)], :dipole, units=Units.theory, seed=0)
 polarize_spins!(sys, (0,0,1))
-
 set_exchange!(sys, -1.0, Bond(1,1,(1,0,0)))
 
 # REWL parameters - won't see much speedup vs. serial WL for small system
 n_wins = 4
 win_overlap = 0.8
 windows = Sunny.get_windows((-2.0, 2.0), n_wins, win_overlap)
-
-# create type for REWL simulation
 REWL = Sunny.ParallelWangLandau(; sys, bin_size=1/L^2, propose=propose_flip, windows)
 
-# sampling parameters
-n_iters = 12 # Small number for testing. Use ≥ 20 in production.
+# Sampling parameters
+n_iters = 18
 max_hchecks_per_iter = 100
 hcheck_interval = 1_000
 exch_interval = 100
 
-# start REWL sampling
+# REWL sampling loop
 for i in 1:n_iters
     for mcs in 1:max_hchecks_per_iter
         Sunny.step_ensemble!(REWL, hcheck_interval, exch_interval)
-
-        # check flatness and start next iteration if satisfied
+        # If flat, go to next iteration
         flat = fill(false, length(REWL.samplers))
         @Threads.threads for i in eachindex(REWL.samplers)
             flat[i] = Sunny.check_flat(REWL.samplers[i].hist; p=0.5)
@@ -48,20 +44,21 @@ end
 E_wins = [Sunny.get_keys(wl.ln_g) for wl in REWL.samplers]
 ln_g_wins = [Sunny.get_vals(wl.ln_g) for wl in REWL.samplers]
 
-# merge into one ln_g(E)
+# Merge into one ln_g(E)
 E, ln_g = Sunny.merge(E_wins, ln_g_wins)
 E .*= L^2
 
-# acceptance rates for replica exchanges
+# Acceptance rates for replica exchanges
 println("A = ", REWL.n_accept ./ REWL.n_exch)
 
-# calculate thermodynamics by reweighting WL results to temperatures
+# Derived thermodynamic properties
 kT = collect(range(0.1, 20, length=1000))
 U = Sunny.ensemble_average(E, ln_g, E, kT)
 U² = Sunny.ensemble_average(E, ln_g, E .^2, kT)
 C = @. (U² - U^2) / kT^2
 
-# plot density of states and thermodynamics
-display( plot(E/L^2, ln_g .- minimum(ln_g), xlabel="E/N", ylabel="ln[g(E)]", legend=false) )
-display( plot(kT, U/L^2, xlabel="kT/J", ylabel="U/N", legend=false) )
-display( plot(kT, C/L^2, xlabel="kT/J", ylabel="C/N", legend=false) )
+# Plots
+fig = Figure()
+lines(fig[1,1], E/L^2, ln_g .- minimum(ln_g); axis=(xlabel="E/N", ylabel="ln[g(E)]"))
+lines(fig[2,1], kT, U/L^2; axis=(xlabel="kT/J", ylabel="U/N"))
+lines(fig[3,1], kT, C/L^2; axis=(xlabel="kT/J", ylabel="C/N"))
