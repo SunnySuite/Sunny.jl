b316fe2214f5b6f6185683cb4a43c8ac2d17bd03
Kipton Barros <kbarros@gmail.com>
2024-03-02 18:19:33 -0700

    Test that all imports are explicit

--- src/Sunny.jl
+++ src/Sunny.jl
@@ -140,9 +140,9 @@ end
 export export_vtk
 
 # Precompile some workloads
-using PrecompileTools
-@setup_workload begin
-    @compile_workload begin
+import PrecompileTools as PT
+PT.@setup_workload begin
+    PT.@compile_workload begin
         latvecs = lattice_vectors(1, 1, 1, 90, 90, 90)
         cryst = Crystal(latvecs, [[0,0,0]], 227, setting="1")
         print_symmetry_table(cryst, 0.8; io=devnull)
