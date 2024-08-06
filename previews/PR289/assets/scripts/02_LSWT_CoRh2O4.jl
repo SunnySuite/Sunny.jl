f5e62d87bf596b29fe8346b17a61ffec1aa07e41
Documenter.jl <documenter@juliadocs.github.io>
2024-08-06 01:59:43 +0000

    build based on 96c1f97

---
+++ previews/PR289/assets/scripts/02_LSWT_CoRh2O4.jl
@@ -0,0 +1,46 @@
+using Sunny, GLMakie
+
+units = Units(:meV)
+a = 8.5031 # (Å)
+latvecs = lattice_vectors(a, a, a, 90, 90, 90)
+cryst = Crystal(latvecs, [[0,0,0]], 227, setting="1")
+
+view_crystal(cryst)
+
+latsize = (1, 1, 1)
+S = 3/2
+J = 0.63 # (meV)
+sys = System(cryst, latsize, [SpinInfo(1; S, g=2)], :dipole; seed=0)
+set_exchange!(sys, J, Bond(1, 3, [0,0,0]))
+
+randomize_spins!(sys)
+minimize_energy!(sys)
+
+@assert energy_per_site(sys) ≈ -2J*S^2
+
+s0 = sys.dipoles[1,1,1,1]
+plot_spins(sys; color=[s'*s0 for s in sys.dipoles])
+
+shape = [0 1 1;
+         1 0 1;
+         1 1 0] / 2
+sys_prim = reshape_supercell(sys, shape)
+@assert energy_per_site(sys_prim) ≈ -2J*S^2
+plot_spins(sys_prim; color=[s'*s0 for s in sys_prim.dipoles])
+
+swt = SpinWaveTheory(sys_prim; measure=ssf_perp(sys_prim))
+
+qs = [[0, 0, 0], [1/2, 0, 0], [1/2, 1/2, 0], [0, 0, 0]]
+path = q_space_path(cryst, qs, 400)
+
+kernel = lorentzian(fwhm=0.8)
+formfactors = [FormFactor("Co2")]
+energies = range(0, 6, 300)
+res = intensities(swt, path; energies, kernel, formfactors)
+plot_intensities(res; units)
+
+radii = range(0, 3, 200) # (1/Å)
+res = powder_average(cryst, radii, 2000) do qs
+    intensities(swt, qs; energies, kernel, formfactors)
+end
+plot_intensities(res; units)
---
+++ previews/PR289/assets/scripts/08_Momentum_Conventions.jl
@@ -0,0 +1,50 @@
+using Sunny, GLMakie
+
+latvecs = lattice_vectors(2, 2, 1, 90, 90, 90)
+cryst = Crystal(latvecs, [[0,0,0]], "P1")
+
+sys = System(cryst, (1, 1, 25), [SpinInfo(1; S=1, g=2)], :dipole; seed=0)
+D = 0.1
+B = 5D
+set_exchange!(sys, dmvec([0, 0, D]), Bond(1, 1, [0, 0, 1]))
+set_field!(sys, [0, 0, B])
+
+randomize_spins!(sys)
+minimize_energy!(sys)
+@assert energy_per_site(sys) ≈ -10D
+
+dt = 0.1
+kT = 0.02
+damping = 0.1
+langevin = Langevin(dt; kT, damping)
+suggest_timestep(sys, langevin; tol=1e-2)
+for _ in 1:10_000
+    step!(sys, langevin)
+end
+
+plot_spins(sys)
+
+sc = dynamical_correlations(sys; dt, nω=100, ωmax=15D)
+add_sample!(sc, sys)
+nsamples = 100
+for _ in 1:nsamples
+    for _ in 1:1_000
+        step!(sys, langevin)
+    end
+    add_sample!(sc, sys)
+end
+density = 100
+path, xticks = reciprocal_space_path(cryst, [[0,0,-1/2], [0,0,+1/2]], density)
+data = intensities_interpolated(sc, path, intensity_formula(sc, :trace; kT));
+
+sys_small = resize_supercell(sys, (1,1,1))
+minimize_energy!(sys_small)
+swt = SpinWaveTheory(sys_small; measure=ssf_trace(sys_small))
+path = q_space_path(cryst, [[0,0,-1/2], [0,0,+1/2]], 400)
+res = intensities_bands(swt, path)
+
+fig = Figure(size=(768, 300))
+ax = Axis(fig[1, 1]; aspect=1.4, title="Classical dynamics", ylabel="Energy")
+heatmap!(ax, 1:size(data, 1), available_energies(sc), data; colorrange=(0.0, 50.0))
+ax = plot_intensities!(fig[1, 2], res; axisopts=(; ylabel="", title="Spin wave theory"))
+fig
