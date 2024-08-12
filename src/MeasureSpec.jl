9676d439eb03524d9bad822f40d8140b0143bc15
Kipton Barros <kbarros@gmail.com>
2024-08-12 17:09:15 -0600

    Update ssf_custom_bm

--- src/MeasureSpec.jl
+++ src/MeasureSpec.jl
@@ -95,40 +95,45 @@ function ssf_custom(f, sys::System; apply_g=true)
 end
 
 """
-    ssf_custom_bm(f, sys::System; u, apply_g=true)
+    ssf_custom_bm(f, sys::System; u, v, apply_g=true)
 
 Specify measurement of the spin structure factor with a custom contraction
-function `f`. The interface is like [`ssf_custom`](@ref), except here the
-function `f` will receive momentum ``𝐪`` and 3×3 structure factor data
+function `f`. The interface is identical to [`ssf_custom`](@ref) except that `f`
+here receives momentum ``𝐪`` and the 3×3 structure factor data
 ``\\mathcal{S}^{αβ}(𝐪, ω)`` in the basis of the Blume-Maleev axis system. The
-wavevector `u`, provided in reciprocal lattice units, lies in the scattering
-plane. In global Cartesian coordinates, the three orthonormal BM axes `(e1, e2,
-e3)` are defined as follows:
+wavevectors `u` and `v`, provided in reciprocal lattice units, will be used to
+define the scattering plane. In global Cartesian coordinates, the three
+orthonormal BM axes `(e1, e2, e3)` are defined as follows:
 
 ```julia
-e1 = normalize(q)      # aligned with momentum transfer q
-e3 = normalize(q × u)  # normal to the scattering plane (q, u)
+e3 = normalize(u × v)  # normal to the scattering plane (u, v)
+e1 = normalize(q)      # momentum transfer q within scattering plane
 e2 = normalize(e3 × q) # perpendicular to q and in the scattering plane
 ```
 
 # Example
 
 ```julia
-# Measure imaginary part of S²³ - S³² in the Blume-Maleev coordinate system.
-# The wavevector u = [0, 0, 1] in RLU may be an incident or final momentum.
-measure = ssf_custom_bm(sys; u=[0, 0, 1]) do q, ssf
+# Measure imaginary part of S²³ - S³² in the Blume-Maleev axis system for
+# the scattering plane [0, K, L].
+measure = ssf_custom_bm(sys; u=[0, 1, 0], v=[0, 0, 1]) do q, ssf
     imag(ssf[2,3] - ssf[3,2])
 end
 ```
 """
-function ssf_custom_bm(f, sys::System; u, apply_g=true)
-    u = orig_crystal(sys).recipvecs * Vec3(u) # to global coordinates
+function ssf_custom_bm(f, sys::System; u, v, apply_g=true)
+    u = orig_crystal(sys).recipvecs * u
+    v = orig_crystal(sys).recipvecs * v
+    e3 = normalize(u × v)
+
     return ssf_custom(sys::System; apply_g) do q, ssf
+        if abs(q ⋅ e3) > 1e-12
+            error("Momentum transfer q not in scattering plane")
+        end
         e1 = normalize(q)      # parallel to q
-        e3 = normalize(q × u)  # normal to the scattering plane
         e2 = normalize(e3 × q) # perpendicular to q, in the scattering plane
         bm = hcat(e1, e2, e3)  # Blume-Maleev axis system
-        f(q' * bm, bm' * ssf * bm)
+        f(Vec3(norm(q), 0, 0), bm' * ssf * bm)
     end
 end
 
