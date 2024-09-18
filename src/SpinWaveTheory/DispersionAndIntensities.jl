13c41c6e2215dc85d740f5cd1c06870a53c6bff2
Kipton Barros <kbarros@gmail.com>
2024-09-17 18:21:38 -0600

    Writing tweak

--- src/SpinWaveTheory/DispersionAndIntensities.jl
+++ src/SpinWaveTheory/DispersionAndIntensities.jl
@@ -253,14 +253,14 @@ to study generalized spiral orders with a single, incommensurate-``𝐤`` orderi
 wavevector. Another alternative [`SpinWaveTheoryKPM`](@ref) is favorable for
 calculations on large magnetic cells, and allows to study systems with disorder.
 An optional nonzero temperature `kT` will scale intensities by the quantum
-thermal occupation factor ``|1 + n_B(ω)|`` where ``n_B(ω) = 1/(exp(βω)-1)`` is
+thermal occupation factor ``|1 + n_B(ω)|`` where ``n_B(ω) = 1/(e^{βω}-1)`` is
 the Bose function.
 
 Intensities can also be calculated for `SampledCorrelations` associated with
 classical spin dynamics. In this case, thermal broadening will already be
 present, and the line-broadening `kernel` becomes optional. Conversely, the
 parameter `kT` becomes required. If positive, it will introduce an intensity
-correction factor ``|βω [1 + n_B(ω)]|`` that undoes the occupation factor for
+correction factor ``|βω (1 + n_B(ω))|`` that undoes the occupation factor for
 the classical Boltzmann distribution and applies the quantum thermal occupation
 factor. The special choice `kT = nothing` will suppress the classical-to-quantum
 correction factor, and yield statistics consistent with the classical Boltzmann
