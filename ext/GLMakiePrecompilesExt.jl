45071c6ed366b4057ad220d117e82c51c28fd0ed
Kipton Barros <kbarros@gmail.com>
2024-10-26 08:18:20 -0600

    Disable GLMakie precompiles on Julia 1.11.1

--- ext/GLMakiePrecompilesExt.jl
+++ ext/GLMakiePrecompilesExt.jl
@@ -1,8 +1,12 @@
 module GLMakiePrecompilesExt
 
 import Sunny, GLMakie
-
 import PrecompileTools as PT
+
+# Julia 1.11.1 caused a change in the ordering of precompilation for extensions.
+# See https://github.com/JuliaLang/julia/issues/56204#issuecomment-2439588043
+@static if VERSION < v"1.11.1"
+
 PT.@setup_workload begin
     PT.@compile_workload begin
         cryst = Sunny.bcc_crystal()
@@ -12,9 +16,11 @@ PT.@setup_workload begin
         inspector = GLMakie.DataInspector(ax; font="Deja Vu Sans Mono")
         GLMakie.Makie.show_data(inspector, plt, 1)
         GLMakie.display(fig; visible=false)
-        
+
         GLMakie.closeall()
     end
 end
+
+end # VERSION < v"1.11.1"
 
 end
