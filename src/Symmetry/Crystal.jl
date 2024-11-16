a387019335a27802ed8917ed3452f268afbbf295
Kipton Barros <kbarros@gmail.com>
2024-11-16 16:09:24 -0700

    Update ref format

--- src/Symmetry/Crystal.jl
+++ src/Symmetry/Crystal.jl
@@ -18,20 +18,21 @@ will be in units of angstrom.
 
 Constructs a crystal from the complete list of atom positions `positions`, with
 coordinates (between 0 and 1) in units of lattice vectors `latvecs`. Spacegroup
-symmetry information is automatically inferred using the Spglib package [1, 2].
-The optional parameter `types` is a list of strings, one for each atom, and can
-be used to break symmetry-equivalence between atoms.
+symmetry information is automatically inferred using the [Spglib
+package](https://github.com/spglib/spglib) [1]. The optional parameter `types`
+is a list of strings, one for each atom, and can be used to break
+symmetry-equivalence between atoms.
 
     Crystal(latvecs, positions, spacegroup; types=nothing, choice=nothing, symprec=1e-5)
 
 Builds a crystal using the symmetries of a `spacegroup`. One representative atom
 must be specified for each occupied Wyckoff. The `spacegroup` may be specified
 as a number 1..230 or as a string, e.g., Hermann–Mauguin or Hall symbol. If only
-a spacegroup number is provided, the ITA standard setting will be employed [3],
-consistent with conventions from the Bilbao crystallographic server [4]. If a
-spacegroup symbol is provided that allows for multiple ITA settings, the
-possible disambiguations will be included in an error message, and may involve a
-`choice` string.
+a spacegroup number is provided, the ITA standard setting [2] will be employed,
+consistent with conventions from the [Bilbao crystallographic
+server](https://www.cryst.ehu.es). If a spacegroup symbol is provided that
+allows for multiple ITA settings, the possible disambiguations will be included
+in an error message, and may involve a `choice` string.
 
 
 # Examples
@@ -66,12 +67,10 @@ See also [`lattice_vectors`](@ref).
 ## References
 
 1. [A. Togo, K. Shinohara, I. Tanaka, _Spglib: a software library for crystal
-   symmetry search_, [arXiv:1809.01590]](https://arxiv.org/abs/1808.01590).
-2. The Spglib package, [available online](https://github.com/spglib/spglib).
-3. International Tables of Crystallography, [Volume
-   A](https://doi.org/10.1107/97809553602060000114).
-4. Wyckoff position listings are available from the [Bilbao crystallographic
-   server](https://www.cryst.ehu.es/cryst/get_wp.html).
+   symmetry search_ (2018)
+   [arXiv:1808.01590]](https://arxiv.org/abs/1808.01590).
+2. [International Tables of Crystallography, Volume A
+   (2016)](https://doi.org/10.1107/97809553602060000114).
 """
 struct Crystal
     root      :: Union{Nothing, Crystal} # Root crystal (invariant under `subcrystal` and reshaping)
