8ca0eca18ec513ffe7bc7ade312c130bb74c1300
Kipton Barros <kbarros@gmail.com>
2025-06-20 13:28:06 -0600

    Tweaks

--- examples/08_Momentum_Conventions.jl
+++ examples/08_Momentum_Conventions.jl
@@ -5,7 +5,7 @@
 # [Boothroyd](https://groups.physics.ox.ac.uk/Boothroyd/PNS/),
 #
 # ```math
-# \mathcal{S}^{α, β}(𝐪, ω) ≡ \frac{1}{2π} \int_{-∞}^{∞} e^{-iωt} ⟨\hat{M}^{†α}_𝐪(0) \hat{M}^β_𝐪(t)⟩ dt.
+# \mathcal{S}^{αβ}(𝐪, ω) ≡ \frac{1}{2π} \int_{-∞}^{∞} e^{-iωt} ⟨\hat{M}^{†α}_𝐪(0) \hat{M}^β_𝐪(t)⟩ dt.
 # ```
 # 
 # The magnetic moment in momentum space ``\hat{𝐌}_𝐪`` is obtained from the
@@ -20,30 +20,32 @@
 # by the number of chemical cells in the sample. For full details, see the
 # documentation page [Structure Factor Conventions](@ref).
 #
-# With appropriate contraction of spin components, ``\mathcal{S}^{α, β}(𝐪, ω)``
-# can be directly related to the neutron scattering cross-section, where ``𝐪``
-# and ``ω`` denote momentum and energy transfer _to_ the sample. For models that
-# lack inversion symmetry, the intensities at ``±𝐪`` may be inequivalent. We
-# illustrate such a case using a 1D chain with competing Ising and
+# With appropriate contraction of spin components, ``\mathcal{S}^{αβ}(𝐪, ω)``
+# can be directly related to the neutron scattering cross-section with ``𝐪``
+# and ``ω`` denoting momentum and energy transfer **to** the sample. For models
+# that lack inversion symmetry, the intensities at ``±𝐪`` may be inequivalent.
+# We illustrate such a case using a 1D chain with competing Ising and
 # Dzyaloshinskii–Moriya couplings between neighboring sites.
 #
-# Be aware that other codes (e.g. [SpinW](https://spinw.org/)) may employ an
-# alternative sign convention that effectively reverses the direction of
-# momentum transfer, ``𝐪 → -𝐪``.
+# Be aware that other codes, e.g. [SpinW](https://spinw.org/), may employ a
+# different Fourier transform convention that effectively reverses the direction
+# of momentum transfer, ``𝐪 → -𝐪``.
+
+# ### Build a 1D model without reflection symmetry
 
 using Sunny, GLMakie
 
-# Selecting the P1 spacegroup will effectively disable all symmetry analysis.
-# This can be a convenient way to avoid symmetry-imposed constraints on the
-# couplings. With this choice, all bonds within the unit cell are treated as
-# symmetry-inequivalent and must be specified independently.
+# The model will live on a 1D chain. Select the spacegroup P1 to effectively
+# disable all symmetry analysis. This eliminates all symmetry-imposed
+# constraints on the couplings. Furthermore, because all bonds become
+# symmetry-inequivalent, each coupling must be specified independently (there is
+# no automatic propagation to "equivalent" bonds).
 
-latvecs = lattice_vectors(2, 2, 1, 90, 90, 90)
+latvecs = lattice_vectors(1, 1, 1, 90, 90, 90)
 cryst = Crystal(latvecs, [[0, 0, 0]], "P1")
 
-# Consider a 1D chain oriented along the lattice vector ``𝐚_3`` such that site
-# ``j`` has position ``𝐫_j = j 𝐚_3``. The Hamiltonian includes DM and
-# Ising-like couplings between nearest neighbors on the chain,
+# Include DM and Ising couplings between nearest neighbors on the chain. Letting
+# ``j`` denote the site position along axis ``𝐚_3``, the full Hamiltonian is
 # ```math
 # ℋ = 𝐃 ⋅ ∑_j 𝐒_j × 𝐒_{j+1} - J ∑_j ∑_j S^z_j S^z_{j+1},
 # ```
@@ -57,29 +59,26 @@ z = [0, 0, 1]
 set_exchange!(sys, dmvec(D * z) - J * z * z', Bond(1, 1, [0, 0, 1]))
 
 # The relatively large Ising coupling favors one of two polarized states,
-# ``±ẑ``. Use [`polarize_spins!`]@(ref) to align spin angular momentum ``𝐒``
-# with the ``+ẑ`` direction, breaking the symmetry by hand. Physically, this
-# polarization could be realized via an applied field ``𝐁`` in the direction
-# ``-ẑ``. This is because ``𝐁`` couples to the [`magnetic_moment`](@ref),
-# which is anti-aligned with ``𝐒``. Visualize the latter using
-# [`plot_spins`](@ref).
+# ``±ẑ``. Use [`polarize_spins!`](@ref) to align ``𝐒`` with ``+ẑ``. This
+# polarization could also be realized via an applied ``𝐁`` field in the
+# _opposite_ direction, as documented in [`set_field!`](@ref).
 
 polarize_spins!(sys, [0, 0, 1])
 @assert energy(sys) ≈ - s^2
 plot_spins(sys)
 
-# ### Calculation using linear spin wave theory
+# ### Calculate intensities from linear spin wave theory
 
 # The [`SpinWaveTheory`](@ref) calculation shows a single band with dispersion
 # ``ϵ(𝐪) = 2 s [J ± D \sin(2πq_3)]`` for the polarization state ``𝐒 = ± s
 # ẑ``. There is a clear dependence on the sign of ``q_3``.
 
-path = q_space_path(cryst, [[0, 0, -1/2], [0, 0, +1/2]], 400)
+path = q_space_path(cryst, [[0, 0, -1/2], [0, 0, 0], [0, 0, +1/2]], 400)
 swt = SpinWaveTheory(sys; measure=ssf_trace(sys))
 res = intensities_bands(swt, path)
 plot_intensities(res; ylims=(0, 5))
 
-# ### Calculation using classical spin dynamics
+# ### Calculate intensities from classical spin dynamics
 
 # Classical dynamics at low temperatures produces, in principle, the same
 # excitation spectrum. Here, finite-size effects will limit ``𝐪``-space
