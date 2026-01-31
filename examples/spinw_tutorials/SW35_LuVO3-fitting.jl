aaed56427ccd9e8b2982078dba02b54460e06d9e
Kipton Barros <kbarros@gmail.com>
2026-01-30 17:00:02 -0700

    Tweak doc

--- examples/spinw_tutorials/SW35_LuVO3-fitting.jl
+++ examples/spinw_tutorials/SW35_LuVO3-fitting.jl
@@ -24,12 +24,12 @@ cryst = Crystal(latvecs, positions, "P b n m")
 
 sys = System(cryst, [1 => Moment(s=1, g=2)], :dipole_uncorrected)
 
-# Exchanges ``J_{ab}`` and ``J_{c}`` are in the (x̂, ŷ) plane and the ẑ
-# direction, respectively. The single-ion anisotropy term has the form ``-
-# \sum_α K_{αα} S_α^2``. Note that a shift of all ``K_{xx}, K_{yy}, K_{zz}`` by
-# some constant ``c`` amounts to a physically irrelevant shift ``- c |S|^2`` of
-# the spin Hamiltonian. Without loss of generality, one can select ``K_{zz} =
-# 0``. With this convention, negative ``K_{xx}`` and ``K_{yy}`` yields an
+# Exchanges ``J_{ab}`` and ``J_{c}`` are in the x̂-ŷ plane and the ẑ
+# direction, respectively. The single-ion anisotropy term has the assumed form
+# ``- \sum_α K_{αα} S_α^2``. Note that a shift of all ``K_{xx}, K_{yy}, K_{zz}``
+# by some constant ``c`` amounts to a physically irrelevant shift ``- c |S|^2``
+# of the spin Hamiltonian. Without loss of generality, one can select ``K_{zz} =
+# 0``. With this convention, negative ``K_{xx}`` and ``K_{yy}`` amount to an
 # easy-axis anisotropy in ``ẑ``. 
 
 set_exchange!(sys, 1.0, Bond(1, 2, (0, 0, 0)), :Jab => 0)
@@ -37,10 +37,10 @@ set_exchange!(sys, 1.0,  Bond(1, 3, (0, 0, 0)), :Jc => 0)
 set_onsite_coupling!(sys, S -> -S[1]^2,  1, :Kxx => 0)
 set_onsite_coupling!(sys, S -> -S[2]^2,  1, :Kyy => 0)
 
-# Set parameters for phase III following Table I of Skoulatos et al. Here, it
+# Parameters for Phase III as reported in Table I of Skoulatos et al. Here, it
 # seems the paper has a typo. To get agreement in the calculated spin wave
-# spectrum, it is necessary to swap the values of their reported exchange
-# parameters, ``J_{ab} = 4.24`` and ``J_{c} = 5.95`` (meV).
+# spectrum, it is necessary to swap the reported exchange parameters, ``J_{ab} =
+# 4.24`` and ``J_{c} = 5.95`` (meV).
 
 labels = [:Jab, :Jc, :Kxx, :Kyy]
 skoulatos_fit = [5.95, 4.24, -0.48, -0.06]
@@ -63,9 +63,9 @@ swt = SpinWaveTheory(sys; measure=ssf_perp(sys))
 res = intensities_bands(swt, path)
 plot_intensities(res)
 
-# This tutorial refits the model parameters using the dispersion data of Fig. 1d
-# of Skoulatos et al. The numbers below were extracted using the
-# [WebPlotDigitizer](https://automeris.io/) tool.
+# This tutorial will refit the model parameters using points on the dispersion
+# curves. The energies below were extracted from Fig. 1d of Skoulatos et al.
+# using the [WebPlotDigitizer](https://automeris.io/) tool.
 
 qs = [
     [0.0, 1.0, 2.0], [0.0, 1.0, 2.1], [0.0, 1.0, 2.2], [0.0, 1.0, 2.3],
@@ -79,11 +79,11 @@ Es = [
     [27.368], [31.798], [33.431]
 ];
 
-# Use [`make_loss_fn`](@ref) to define an optimization target. Do _not_ use
-# [`minimize_energy!`](@ref) within the loss function because the magnetic
-# structure is known experimentally to be Néel. Sunny provides
-# `labeled_peaks_mismatch` for robust comparison of the predicted bands with the
-# experimentally measured intensity peaks. 
+# Use [`make_loss_fn`](@ref) to define an optimization target. The system
+# already has the correct Néel magnetic order; to keep it fixed, the loss
+# function should _not_ call [`minimize_energy!`](@ref). Sunny provides
+# `labeled_peaks_mismatch` for robust comparison of excitation energies. The
+# parameter `σ` is uncertainty of the experimental energies.
 
 loss = make_loss_fn(sys, labels) do sys
     swt = SpinWaveTheory(sys; measure=ssf_perp(sys))
@@ -91,7 +91,7 @@ loss = make_loss_fn(sys, labels) do sys
     Sunny.labeled_peaks_mismatch(res, Es; σ=0.2)
 end;
 
-# Select some relatively non-informative initial guess. Measure its loss
+# Select some relatively non-informative parameter guess. Measure its loss
 # (fitting mismatch) as a baseline.
 
 guess = [3, 3, -0.2, -0.1] # Guess for [Jab, Jc, Kxx, Kyy]
@@ -101,8 +101,7 @@ loss(guess)
 # variety of powerful optimization methods. The particle swarm method is
 # gradient free, has some ability to overcome local minima, and supports
 # specification of lower and upper bounds on the optimization variables. Use 10
-# independent runs of particle swarm to search for the most promising model
-# structure.
+# independent runs of particle swarm to search for a good model.
 
 import Optim
 
