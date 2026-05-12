1711a94a81af78c5fafbeaf94fe1ea586f53f257
Documenter.jl <documenter@juliadocs.github.io>
2026-05-12 17:02:29 +0000

    delete history

---
+++ v0.7.0/assets/scripts/SW14_YVO3.jl
@@ -0,0 +1,40 @@
+using Sunny, GLMakie
+
+units = Units(:meV, :angstrom)
+a = 5.2821 / sqrt(2)
+b = 5.6144 / sqrt(2)
+c = 7.5283
+latvecs = lattice_vectors(a, b, c, 90, 90, 90)
+positions = [[0, 0, 0], [0, 0, 1/2]]
+types = ["V", "V"]
+cryst = Crystal(latvecs, positions, 1; types)
+
+sys = System(cryst, [1 => Moment(s=1/2, g=2), 2 => Moment(s=1/2, g=2)], :dipole_large_s; dims=(2,2,1))
+Jab = 2.6
+Jc  = 3.1
+δ   = 0.35
+K1  = 0.90
+K2  = 0.97
+d   = 1.15
+Jc1 = [-Jc*(1+δ)+K2 0 -d; 0 -Jc*(1+δ) 0; +d 0 -Jc*(1+δ)]
+Jc2 = [-Jc*(1-δ)+K2 0 +d; 0 -Jc*(1-δ) 0; -d 0 -Jc*(1-δ)]
+set_exchange!(sys, Jab, Bond(1, 1, [1, 0, 0]))
+set_exchange!(sys, Jab, Bond(2, 2, [1, 0, 0]))
+set_exchange!(sys, Jc1, Bond(1, 2, [0, 0, 0]))
+set_exchange!(sys, Jc2, Bond(2, 1, [0, 0, 1]))
+set_exchange!(sys, Jab, Bond(1, 1, [0, 1, 0]))
+set_exchange!(sys, Jab, Bond(2, 2, [0, 1, 0]))
+set_onsite_coupling!(sys, S -> -K1*S[1]^2, 1)
+set_onsite_coupling!(sys, S -> -K1*S[1]^2, 2)
+
+view_crystal(sys)
+
+randomize_spins!(sys)
+minimize_energy!(sys)
+plot_spins(sys)
+
+swt = SpinWaveTheory(sys; measure=ssf_perp(sys))
+qs = [[0.75, 0.75, 0], [0.5, 0.5, 0], [0.5, 0.5, 1]]
+path = q_space_path(cryst, qs, 400)
+res = intensities_bands(swt, path)
+plot_intensities(res; units)
---
+++ v0.7.5/assets/scripts/SW11_La2CuO4.jl
@@ -0,0 +1,34 @@
+using Sunny, GLMakie
+@assert pkgversion(Sunny) >= v"0.7.5"
+
+units = Units(:meV, :angstrom)
+latvecs = lattice_vectors(1, 1, 10, 90, 90, 90)
+positions = [[0, 0, 0]]
+types = ["Cu"]
+cryst = Crystal(latvecs, positions, 139; types)
+view_crystal(cryst; ndims=2)
+
+sys = System(cryst, [1 => Moment(s=1/2, g=2)], :dipole; dims=(2, 2, 1))
+J   = 138.3
+Jp  = 2
+Jpp = 2
+Jc  = 38
+set_exchange!(sys, J-Jc/2, Bond(1, 1, [1, 0, 0]))
+set_exchange!(sys, Jp-Jc/4, Bond(1, 1, [1, 1, 0]))
+set_exchange!(sys, Jpp, Bond(1, 1, [2, 0, 0]))
+
+randomize_spins!(sys)
+minimize_energy!(sys)
+plot_spins(sys; ndims=2)
+
+qs = [[3/4,1/4,0], [1/2, 1/2, 0], [1/2, 0, 0], [3/4, 1/4, 0], [1,0,0], [1/2 0 0]]
+labels = ["P", "M", "X", "P", "Γ", "X"]
+path = q_space_path(cryst, qs, 400; labels)
+energies = range(0, 320, 400)
+swt = SpinWaveTheory(sys; measure=ssf_perp(sys))
+res = intensities(swt, path; energies, kernel=gaussian(fwhm=35))
+res.energies .*= 1.18
+plot_intensities(res; units)
+
+res = intensities_static(swt, path)
+plot_intensities(res; colorrange=(0,20), units)
