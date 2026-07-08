4f1d93eb1e9a7f54bf9f32c931a564876c630e41
ddahlbom <david.dahlbom@gmail.com>
2026-07-07 22:35:55 -0400

    More careful straddling test

--- src/EntangledUnits/EntangledReshaping.jl
+++ src/EntangledUnits/EntangledReshaping.jl
@@ -20,13 +20,9 @@ function units_for_reshaped_system(reshaped_sys_uncontracted, esys)
     while length(new_atoms) > 0
         # Pick any site from list of new sites
         new_atom = new_atoms[1]
-        new_site = (1, 1, 1, new_atom) # Any representative cell works.
-        new_site_global = global_position_at(reshaped_sys_uncontracted, new_site)
 
-        # Map the reshaped site back to an atom in the original crystal.
-        new_site_in_original_basis = sys_uncontracted.crystal.latvecs \ new_site_global
-        original_site = position_to_site(sys_uncontracted, new_site_in_original_basis)
-        original_atom = original_site[4]
+        # Get the corresponding atom in the original system
+        original_atom = map_atom_to_other_system(new_crystal, new_atom, sys_uncontracted)[4]
 
         # Find the original unit containing this atom.
         unit = units[findfirst(unit -> original_atom in unit, units)]
@@ -35,17 +31,16 @@ function units_for_reshaped_system(reshaped_sys_uncontracted, esys)
         # unit, then apply those displacements to the reshaped reference site.
         original_reference_position = position_at(sys_uncontracted, (1, 1, 1, original_atom))
         unit_displacements = [position_at(sys_uncontracted, (1, 1, 1, atom)) - original_reference_position for atom in unit]
-        unit_displacements_global = [sys_uncontracted.crystal.latvecs * displacement for displacement in unit_displacements]
 
         # Map each target position back to a reshaped-crystal atom + lattice
         # offset. Any nonzero offset means the unit was split across cells.
-        new_unit_sites = map(unit_displacements_global) do displacement_global
-            target_global = new_site_global + displacement_global
+        new_unit_atoms_and_offsets = map(unit_displacements) do displacement
+            target_global = orig_crystal(sys_uncontracted).latvecs*(original_reference_position + displacement)
             target_in_reshaped_basis = reshaped_sys_uncontracted.crystal.latvecs \ target_global
             position_to_atom_and_offset(reshaped_sys_uncontracted.crystal, target_in_reshaped_basis)
         end
         new_unit = Int64[]
-        for (a, lattice_offset) in new_unit_sites
+        for (a, lattice_offset) in new_unit_atoms_and_offsets
             if !allequal(lattice_offset)
                 error("Given shape incompatible with entangled unit structure. Unit split between crystallographic cells.")
             end
